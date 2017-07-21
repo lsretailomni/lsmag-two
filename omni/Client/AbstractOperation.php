39fd146e3a6d471589e307d2b065051d2d4746cf
Florin Godard <florin@lsretail.com>
2017-07-21 15:47:45 +0000

    Small refactoring in AbstractOperation

--- omni/Client/AbstractOperation.php
+++ omni/Client/AbstractOperation.php
@@ -8,9 +8,14 @@ abstract class AbstractOperation implements IOperation
 {
     /** @var  ServiceType */
     protected $service_type;
+    protected $logger;
+    protected $writer;
 
     public function __construct ( ServiceType $service_type ) {
         $this->service_type = $service_type;
+        $this->writer = new \Zend\Log\Writer\Stream(BP . '/var/log/omniclient.log');
+        $this->logger = new \Zend\Log\Logger();
+        $this->logger->addWriter($this->writer);
     }
 
     /**
@@ -41,19 +46,17 @@ abstract class AbstractOperation implements IOperation
         $isDeveloperMode = \Magento\Framework\App\State::MODE_DEVELOPER === $state->getMode();
 
         if ($isDeveloperMode) {
-            $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/omniclient.log');
-            $logger = new \Zend\Log\Logger();
-            $logger->addWriter($writer);
-            $logger->debug("==== REQUEST == ".date("Y-m-d H:i:s O")." == ".$operation_name." ====");
-            $logger->debug($this->formatXML($this->getClient()->getLastRequest()));
-            $logger->debug("==== RESPONSE == ".date("Y-m-d H:i:s O")." == ".$operation_name." ====");
-            $logger->debug($this->formatXML($this->getClient()->getLastResponse()));
+            $this->logger->debug("==== REQUEST == ".date("Y-m-d H:i:s O")." == ".$operation_name." ====");
+            $this->logger->debug($this->formatXML($this->getClient()->getLastRequest()));
+            $this->logger->debug("==== RESPONSE == ".date("Y-m-d H:i:s O")." == ".$operation_name." ====");
+            $this->logger->debug($this->formatXML($this->getClient()->getLastResponse()));
         }
     }
 
     protected function makeRequest ( $operation_name ) {
         $request_input = $this->getOperationInput();
         $client = $this->getClient();
+        // TODO: add error handling, maybe parse Nav errors into new NavException?
         $response = $client->{$operation_name}( $request_input );
         $this->debugLog($operation_name);
         return $response;
