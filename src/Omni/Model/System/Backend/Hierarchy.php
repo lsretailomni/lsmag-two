3ee37e26b4196704d5a842673caa6f779729be2c
Zeeshan khuwaja <zeeshan.khuwaja@lsretail.com>
2018-11-08 17:00:19 +0800

    [OMNI-4679] Fixed Hierarchy Issue in the backend

--- src/Omni/Model/System/Backend/Hierarchy.php
+++ src/Omni/Model/System/Backend/Hierarchy.php
@@ -9,20 +9,12 @@ use Magento\Framework\Model\Context;
 use Magento\Framework\Model\ResourceModel\AbstractResource;
 use Magento\Framework\Registry;
 use Psr\Log\LoggerInterface;
-use Ls\Replication\Cron\ReplEcommHierarchyTask;
 
-class NavStore extends Value
+class Hierarchy extends Value
 {
     /** @var LoggerInterface */
     protected $logger;
 
-    protected $_replicationHelper;
-
-
-    /**
-     * @var ReplEcommHierarchyTask
-     */
-    protected $_replHierarchyTask;
 
     /**
      * @param Context               $context
@@ -39,30 +31,22 @@ class NavStore extends Value
                                   TypeListInterface $cacheTypeList,
                                   AbstractResource $resource = NULL,
                                   AbstractDb $resourceCollection = NULL,
-                                  ReplEcommHierarchyTask $replEcommHierarchyTask,
-                                  \Ls\Replication\Helper\ReplicationHelper $replicationHelper,
                                   array $data = [ ]
     ) {
         $this->logger = $context->getLogger();
-        $this->_replHierarchyTask   =   $replEcommHierarchyTask;
-        $this->_replicationHelper   =   $replicationHelper;
         parent::__construct( $context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data );
     }
 
-    public function beforeSave () {
 
-        return parent::beforeSave();
+    public function isValueChanged(){
+        if($this->getValue() == ''){
+            return false;
+
+        }else{
+            return parent::isValueChanged();
+        }
     }
 
-    public function afterSave () {
 
-
-        return parent::afterSave();
-    }
-
-    public function afterDelete () {
-
-        return parent::afterDelete();
-    }
 
 }
