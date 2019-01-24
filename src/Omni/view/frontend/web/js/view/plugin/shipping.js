fb5a39ee529b5aca61f029e356757b7bd52899ea
Umar Yousaf <umaryo@lsretail.com>
2019-01-24 17:26:49 +0800

    Commenting the pickup date from all places plus updating the label if product is not available in any store

--- src/Omni/view/frontend/web/js/view/plugin/shipping.js
+++ src/Omni/view/frontend/web/js/view/plugin/shipping.js
@@ -10,7 +10,8 @@ define([
             validateShippingInformation: function () {
                 if( quote.shippingMethod().carrier_code == 'clickandcollect') {
                     var stores = $.parseJSON(window.checkoutConfig.shipping.select_store.stores);
-                    if ($('#pickup-date').val() == '' || (stores.totalRecords > 1 && $('#pickup-store').val() == '')) {
+                    // if ($('#pickup-date').val() == '' || (stores.totalRecords > 1 && $('#pickup-store').val() == '')) {
+                    if (stores.totalRecords > 1 && $('#pickup-store').val() == '') {
                         this.errorValidationMessage('Please provide when and where (if suitable) you prefer to pick your order.');
                         return false;
                     }
