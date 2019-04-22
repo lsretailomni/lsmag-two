d134f6b25017e6a7b2e7d2a6c188b130eb567030
Sudhanshu Bajaj <sudhanshu.b@lsretail.com>
2019-04-22 10:57:44 +0800

    [OMNI-4822] Improvements and changes

--- src/Omni/Plugin/Checkout/Block/Checkout/AttributeMerger/StreetPlugin.php
+++ src/Omni/Plugin/Checkout/Block/Checkout/AttributeMerger/StreetPlugin.php
@@ -20,12 +20,12 @@ class StreetPlugin
             $result['street']['children'][1]['placeholder'] = __('Street Name/Landmark');
             $result['street']['children'][0]['validation'] = [
                 'required-entry' => true,
-                'min_text_len‌​gth' => 1,
+                'min_text_len​gth' => 1,
                 'max_text_length' => 50
             ];
             $result['street']['children'][1]['validation'] = [
                 'required-entry' => false,
-                'min_text_len‌​gth' => 1,
+                'min_text_length' => 1,
                 'max_text_length' => 50
             ];
             $result['city']['validation'] = [
