53a66a26fbc5e75cc97a1d58386acf00f15a1795
Zeeshan Khuwaja <zeeshan.khuwaja@lsretail.com>
2019-05-21 18:56:44 +0800

    [OMNI-4884] FIx issue for configurable products without variants.

--- src/Replication/Cron/ProductCreateTask.php
+++ src/Replication/Cron/ProductCreateTask.php
@@ -303,6 +303,8 @@ class ProductCreateTask
                     } else {
                         $product->setPrice($item->getUnitPrice());
                     }
+
+
                     $product->setAttributeSetId(4);
                     $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
                     $product->setTypeId(\Magento\Catalog\Model\Product\Type::TYPE_SIMPLE);
@@ -1073,6 +1075,10 @@ class ProductCreateTask
                     $itemPrice = $this->getItemPrice($value->getItemId(), $value->getVariantId());
                     if (isset($itemPrice)) {
                         $productData->setPrice($itemPrice->getUnitPrice());
+                    }else{
+                        // Just incase if we dont have variant specific price then we have to use the default price.
+                        $itemPrice = $this->getItemPrice($value->getItemId());
+                        $productData->setPrice($itemPrice->getUnitPrice());
                     }
                     $productImages = $this->replicationHelper->getImageLinksByType(
                         $value->getItemId() . ',' . $value->getVariantId(),
@@ -1109,7 +1115,9 @@ class ProductCreateTask
                 if (isset($itemPrice)) {
                     $productV->setPrice($itemPrice->getUnitPrice());
                 } else {
-                    $productV->setPrice($item->getUnitPrice());
+                    // Just incase if we dont have price for Variant then in that case we are using the price of main product.
+                    $itemPrice = $this->getItemPrice($value->getItemId(), $value->getVariantId());
+                    $productV->setPrice($itemPrice->getUnitPrice());
                 }
                 $productV->setAttributeSetId(4);
                 $productV->setWebsiteIds([1]);
