7bd6d3b1bee41ed13482107b9a6a5b13c2c0347c
Sudhanshu Bajaj <sudhanshu.b@lsretail.com>
2019-06-13 14:55:11 +0800

    [OMNI-4883] Add meta data to Replication tables

--- src/Replication/Setup/UpgradeSchema/ReplAttribute.php
+++ src/Replication/Setup/UpgradeSchema/ReplAttribute.php
@@ -18,23 +18,22 @@ class ReplAttribute
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $table_name = $setup->getTable( 'ls_replication_repl_attribute' ); 
-        if ( ! $setup->tableExists( $table_name ) ) {
-
+        if(!$setup->tableExists($table_name)) {
         	$table = $setup->getConnection()->newTable( $table_name );
-
-        	$table->addColumn( 'repl_attribute_id', Table::TYPE_INTEGER, NULL, 
+        	$table->addColumn('repl_attribute_id', Table::TYPE_INTEGER, NULL, 
         	                    [ 'identity' => TRUE, 'primary' => TRUE,
-        	                      'unsigned' => TRUE, 'nullable' => FALSE, 'auto_increment'=> TRUE ] );
-        	$table->addColumn( 'scope', Table::TYPE_TEXT, 8);
-        	$table->addColumn( 'scope_id', Table::TYPE_INTEGER, 11);
-        	$table->addColumn( 'processed', Table::TYPE_BOOLEAN, null, [ 'default' => 0 ],'flag to check if data is already coped into magento 0 means needs to be copied into Magento tables, 1 means already copied' );
-        	$table->addColumn( 'is_updated', Table::TYPE_BOOLEAN, null, [ 'default' => 0 ],'flag to check if data is already updated from Omni into magento 0 means already updated, 1 means  needs to be updated into Magento tables' );
-        	$table->addColumn( 'Code' , Table::TYPE_TEXT, '' );
-        	$table->addColumn( 'DefaultValue' , Table::TYPE_TEXT, '' );
-        	$table->addColumn( 'Description' , Table::TYPE_TEXT, '' );
-        	$table->addColumn( 'IsDeleted' , Table::TYPE_BOOLEAN, '' );
-        	$table->addColumn( 'ValueType' , Table::TYPE_INTEGER, '' );
-
+        	                      'unsigned' => TRUE, 'nullable' => FALSE, 'auto_increment'=> TRUE ]);
+        	$table->addColumn('scope', Table::TYPE_TEXT, 8);
+        	$table->addColumn('scope_id', Table::TYPE_INTEGER, 11);
+        	$table->addColumn('processed', Table::TYPE_BOOLEAN, null, [ 'default' => 0 ], 'Flag to check if data is already copied into Magento. 0 means needs to be copied into Magento tables & 1 means already copied');
+        	$table->addColumn('is_updated', Table::TYPE_BOOLEAN, null, [ 'default' => 0 ], 'Flag to check if data is already updated from Omni into Magento. 0 means already updated & 1 means needs to be updated into Magento tables');
+        	$table->addColumn('Code' , Table::TYPE_TEXT, '');
+        	$table->addColumn('DefaultValue' , Table::TYPE_TEXT, '');
+        	$table->addColumn('Description' , Table::TYPE_TEXT, '');
+        	$table->addColumn('IsDeleted' , Table::TYPE_BOOLEAN, '');
+        	$table->addColumn('ValueType' , Table::TYPE_INTEGER, '');
+        	$table->addColumn('created_at', Table::TYPE_TIMESTAMP, null, [ 'nullable' => false, 'default' => Table::TIMESTAMP_INIT ], 'Created At');
+        	$table->addColumn('updated_at', Table::TYPE_TIMESTAMP, null, [ 'nullable' => false, 'default' => Table::TIMESTAMP_INIT_UPDATE ], 'Updated At');
         	$setup->getConnection()->createTable( $table );
         }
     }
