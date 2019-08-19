1d340a22c326ba461a40559aa7173f7741b191e2
Syed Waqas Nabi <syedna@lsretail.com>
2019-08-19 17:53:35 +0800

    [OMNI-4936] Customization the Custom Grids in Admin

---
+++ src/Replication/Ui/Component/Listing/Column/Stores.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ls\Replication\Ui\Component\Listing\Column;
+
+use Magento\Store\Model\System\Store as StoreManager;
+
+/**
+ * Class ColorStatus
+ * @package Ls\Replication\Ui\Component\Listing\Column
+ */
+class Stores extends \Magento\Ui\Component\Listing\Columns\Column
+{
+    /**
+     * @var \Magento\Store\Model\System\Store
+     */
+    public $storeManager;
+
+    /**
+     * Stores constructor.
+     * @param \Magento\Framework\View\Element\UiComponent\ContextInterface $context
+     * @param \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory
+     * @param StoreManager $storeManager
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\UiComponent\ContextInterface $context,
+        \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory,
+        StoreManager $storeManager,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->storeManager = $storeManager;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if ($this->getData('name') == "scope_id") {
+                    $item[$this->getData('name')]= $this->storeManager->getStoreName($item['scope_id']);
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
