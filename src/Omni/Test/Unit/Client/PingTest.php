90b60cfdc520771d8b8ba914a6073e591c218be6
Sudhanshu Bajaj <sudhanshu.b@lsretail.com>
2019-10-23 15:53:59 +0800

    [OMNI-5007] Service URL Validate & Ping Pong

--- src/Omni/Test/Unit/Client/PingTest.php
+++ src/Omni/Test/Unit/Client/PingTest.php
@@ -2,7 +2,10 @@
 
 namespace Ls\Omni\Test\Unit\Client;
 
-use SoapClient;
+use Ls\Omni\Client\Ecommerce\ClassMap;
+use Ls\Omni\Service\ServiceType;
+use \Ls\Omni\Service\Soap\Client as OmniClient;
+use Zend\Uri\UriFactory;
 
 class PingTest extends \PHPUnit\Framework\TestCase
 {
@@ -10,16 +13,18 @@ class PingTest extends \PHPUnit\Framework\TestCase
 
     protected function setUp()
     {
-        $baseUrl = 'http://10.27.9.39/LSOmniService411';
-        $url = implode('/', [$baseUrl, 'UCService.svc']);
-        $this->client = new SoapClient(
-            $url . '?singlewsdl',
-            ['features' => SOAP_SINGLE_ELEMENT_ARRAYS]
-        );
+        $baseUrl = $_ENV['BASE_URL'];
+        $url = implode('/', [$baseUrl, 'UCService.svc?singlewsdl']);
+        $service_type = new ServiceType(ServiceType::ECOMMERCE);
+        $uri = UriFactory::factory($url);
+        $this->client = new OmniClient($uri, $service_type);
+        $this->client->setClassmap(ClassMap::getClassMap());
     }
 
     public function testValidateBaseUrl()
     {
         $this->assertNotNull($this->client);
+        $pong = $this->client->Ping();
+        $this->assertContains('PONG OK> Successfully connected to [LSOmni DB] & [LSCentral DB]', $pong->getResult());
     }
 }
--- src/Omni/Test/Unit/Service/ServiceTest.php
+++ src/Omni/Test/Unit/Service/ServiceTest.php
@@ -2,8 +2,19 @@
 
 namespace Ls\Omni\Test\Unit\Service;
 
+use \Ls\Core\Model\LSR;
+use Ls\Omni\Client\Ecommerce\Operation\Ping;
+use \Ls\Omni\Service\Service;
+use \Ls\Omni\Service\ServiceType;
+use Magento\Catalog\Model\CategoryRepository;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+
 class ServiceTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var Service
+     */
+    protected $model;
 
     /**
      * @var \Magento\Framework\App\Config|\PHPUnit_Framework_MockObject_MockObject
@@ -11,30 +22,31 @@ class ServiceTest extends \PHPUnit\Framework\TestCase
     protected $config;
 
     /**
-     * @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
+     * @var LSR|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_objectManager;
-
-    /**
-     * @var \Ls\Core\Model\LSR
-     */
-    protected $lsr;
+    protected $lsrMock;
 
     const BASE_URL = 'http://10.27.9.39/LSOmniService411';
 
     protected function setUp()
     {
+        $objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->lsrMock = $this->createMock(LSR::class);
+        $this->serviceMock = $this->createMock(Service::class);
         $this->config = $this->createMock(\Magento\Framework\App\Config::class);
         $scope = \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT;
-        $this->_objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->model = $objectManager->getObject(
+            Service::class
+        );
     }
 
     public function testLsr()
     {
-        $lsrMock = $this->createMock(\Ls\Core\Model\LSR::class);
-        $lsrMock->method('getStoreConfig')
+        $this->lsrMock
+            ->method('getStoreConfig')
             ->with('ls_mag/service/base_url')
             ->willReturn(self::BASE_URL);
-        $this->assertEquals(self::BASE_URL, $lsrMock->getStoreConfig('ls_mag/service/base_url'));
+        $service_type = new ServiceType( ServiceType::ECOMMERCE );
+        $this->assertEquals(self::BASE_URL.'/UCService.svc?singlewsdl', $this->model->getUrl($service_type,self::BASE_URL));
     }
 }
