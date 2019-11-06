b5b12c4a4501fc4d8992f0a4ea34dce5b1724c0e
Syed Waqas Nabi <syedna@lsretail.com>
2019-11-06 13:02:54 +0800

    [OMNI-5019] Process Order Payments from LS Central

--- src/Webhooks/Model/OrderPayment.php
+++ src/Webhooks/Model/OrderPayment.php
@@ -2,7 +2,17 @@
 
 namespace Ls\WebHooks\Model;
 
+use Exception;
 use \Ls\Webhooks\Api\OrderPaymentInterface;
+use \Ls\Webhooks\Helper\Data;
+use \Ls\Webhooks\Logger\Logger;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\DB\TransactionFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order\Email\Sender\InvoiceSender;
+use Magento\Sales\Model\Service\InvoiceService;
 
 /**
  * Class OrderPayment
@@ -10,28 +20,71 @@ use \Ls\Webhooks\Api\OrderPaymentInterface;
  */
 class OrderPayment implements OrderPaymentInterface
 {
-    const SUCCESS = "OK";
-    const ERROR = "ERROR";
 
     /**
-     * @var \Ls\Webhooks\Logger\Logger
+     * @var Logger
      */
     public $logger;
 
     /**
+     * @var OrderRepositoryInterface
+     */
+    public $orderRepository;
+
+    /**
+     * @var InvoiceService
+     */
+    public $invoiceService;
+
+    /**
+     * @var TransactionFactory
+     */
+    public $transactionFactory;
+
+    /**
+     * @var InvoiceSender
+     */
+    public $invoiceSender;
+
+    /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
+    public $searchCriteriaBuilder;
+
+    /**
+     * @var Data
+     */
+    public $helper;
+
+    /**
      * OrderPayment constructor.
-     * @param \Ls\Webhooks\Logger\Logger $logger
+     * @param Logger $logger
+     * @param OrderRepositoryInterface $orderRepository
+     * @param InvoiceService $invoiceService
+     * @param TransactionFactory $transactionFactory
+     * @param ManagerInterface $messageManager
+     * @param InvoiceSender $invoiceSender
      */
     public function __construct(
-        \Ls\Webhooks\Logger\Logger $logger
+        Logger $logger,
+        OrderRepositoryInterface $orderRepository,
+        InvoiceService $invoiceService,
+        TransactionFactory $transactionFactory,
+        ManagerInterface $messageManager,
+        InvoiceSender $invoiceSender,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        Data $helper
     ) {
-        $this->logger = $logger;
+
+        $this->logger                = $logger;
+        $this->orderRepository       = $orderRepository;
+        $this->invoiceService        = $invoiceService;
+        $this->transactionFactory    = $transactionFactory;
+        $this->invoiceSender         = $invoiceSender;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->helper                = $helper;
     }
 
     /**
      * set order status Api.
-     *
-     * @api
      *
      * @param string $document_id
      * @param string $status (Unchanged=0,Changed=1,Cancelled=2)
@@ -39,24 +92,29 @@ class OrderPayment implements OrderPaymentInterface
      * @param double $amount
      *
      * @return String
+     * @api
+     *
      */
-    public function set($document_id, $status, $token, $amount)
+    public function set($documentId, $status, $token, $amount)
     {
         try {
-            $data=[
-                "document_id"   => $document_id,
-                "status"        => $status,
-                "token"         => $token,
-                "amount"        => $amount
+            $data = [
+                "document_id" => $documentId,
+                "status"      => $status,
+                "token"       => $token,
+                "amount"      => $amount
             ];
-            $this->logger->info("OrderPayment", $data);
-            return self::SUCCESS;
-        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            $this->logger->info("orderpayment", $data);
+            if (!empty($documentId)) {
+                $result = $this->helper->generateInvoice($data);
+                return $result;
+            }
+        } catch (LocalizedException $e) {
             $this->logger->error($e->getMessage());
-            return self::ERROR;
-        } catch (\Exception $e) {
+            return $this->helper::ERROR;
+        } catch (Exception $e) {
             $this->logger->error($e->getMessage());
-            return self::ERROR;
+            return $this->helper::ERROR;
         }
     }
 }
