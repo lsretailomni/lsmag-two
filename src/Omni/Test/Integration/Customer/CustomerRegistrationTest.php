069e415e8d36509dc295bd32b91561731b7f84c7
Syed Waqas Nabi <syedna@lsretail.com>
2020-04-01 03:18:05 +0800

    [OMNI-5008] Integration test for customer registration.

---
+++ src/Omni/Test/Integration/Customer/CustomerRegistrationTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Ls\Omni\Test\Customer;
+
+use \Ls\Omni\Client\Ecommerce\ClassMap;
+use \Ls\Omni\Client\Ecommerce\Entity\Account;
+use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfAddress;
+use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfCard;
+use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfNotification;
+use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfOneList;
+use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfProfile;
+use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfPublishedOffer;
+use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfSalesEntry;
+use \Ls\Omni\Client\Ecommerce\Entity\ContactCreate;
+use \Ls\Omni\Client\Ecommerce\Entity\MemberContact;
+use \Ls\Omni\Helper\ContactHelper;
+use \Ls\Omni\Service\ServiceType;
+use \Ls\Omni\Service\Soap\Client as OmniClient;
+use Magento\Eav\Api\AttributeRepositoryInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\TestCase\AbstractController;
+use Zend\Uri\UriFactory;
+
+class CustomerRegistrationTest extends AbstractController
+{
+    /** @var OmniClient */
+    public $client;
+
+    public $attributeRepository;
+
+    public $contactHelper;
+
+    protected function setUp()
+    {
+        $baseUrl      = $_ENV['BASE_URL'];
+        $url          = implode('/', [$baseUrl, 'UCService.svc?singlewsdl']);
+        $service_type = new ServiceType(ServiceType::ECOMMERCE);
+        $uri          = UriFactory::factory($url);
+        $this->client = new OmniClient($uri, $service_type);
+        $this->client->setClassmap(ClassMap::getClassMap());
+        $this->assertNotNull($this->client);
+
+        $this->attributeRepository = Bootstrap::getObjectManager()->get(AttributeRepositoryInterface::class);
+        $this->contactHelper       = Bootstrap::getObjectManager()->get(ContactHelper::class);
+    }
+
+    private function testCustomerRegistrationOmni()
+    {
+        $this->assertNotNull($this->client);
+        $append      = "test4" . chr(rand(97, 122));
+        $alternateId = 'LSM' . str_pad(md5(rand(500, 600) . $append . $_ENV['USERNAME']), 8, '0', STR_PAD_LEFT);
+        $contact     = new MemberContact();
+        $contact->setAlternateId($alternateId);
+        $contact->setEmail($append . $_ENV['EMAIL']);
+        $contact->setUserName($append . $_ENV['USERNAME']);
+        $contact->setPassword($_ENV['PASSWORD']);
+        $contact->setFirstName("test");
+        $contact->setLastName("test");
+
+        $contactCreate = new ContactCreate();
+        $contactCreate->setContact($contact);
+
+        $response = $this->client->ContactCreate($contactCreate);
+        $result   = $response->getResult();
+        $this->assertInstanceOf(MemberContact::class, $result);
+        $this->assertInstanceOf(ArrayOfAddress::class, $result->getAddresses());
+        $this->assertInstanceOf(ArrayOfCard::class, $result->getCards());
+        $this->assertInstanceOf(ArrayOfNotification::class, $result->getNotifications());
+        $this->assertInstanceOf(ArrayOfOneList::class, $result->getOneLists());
+        $this->assertInstanceOf(ArrayOfProfile::class, $result->getProfiles());
+        $this->assertInstanceOf(ArrayOfPublishedOffer::class, $result->getPublishedOffers());
+        $this->assertInstanceOf(ArrayOfSalesEntry::class, $result->getSalesEntries());
+        $this->assertInstanceOf(Account::class, $result->getAccount());
+        $this->assertNotNull($result->getUserName());
+        $this->assertNotNull($result->getEmail());
+        $this->assertNotNull($result->getId());
+        return $result;
+    }
+
+    public function testCustomerRegistration()
+    {
+        $contact           = $this->testCustomerRegistrationOmni();
+        $attributeUsername = $this->attributeRepository->get('customer', 'lsr_username');
+        $this->assertEquals('lsr_username', $attributeUsername->getAttributeCode());
+        $attributeLsrId = $this->attributeRepository->get('customer', 'lsr_id');
+        $this->assertEquals('lsr_id', $attributeLsrId->getAttributeCode());
+        $attributeLsrToken = $this->attributeRepository->get('customer', 'lsr_token');
+        $this->assertEquals('lsr_token', $attributeLsrToken->getAttributeCode());
+        $attributeLsrCardId = $this->attributeRepository->get('customer', 'lsr_cardid');
+        $this->assertEquals('lsr_cardid', $attributeLsrCardId->getAttributeCode());
+        $this->testGetCustomerGroupIdByName($contact->getAccount()->getScheme()->getId());
+    }
+
+    private function testGetCustomerGroupIdByName($groupName)
+    {
+        $customerGroupId = $this->contactHelper->getCustomerGroupIdByName($groupName);
+        $this->assertNotNull($customerGroupId);
+    }
+}
