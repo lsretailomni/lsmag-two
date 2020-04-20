dd3465234a39c408e70d5e1e21bd3899289523b1
Umar Yousaf <umar.yousaf@briteskies.com>
2020-04-20 11:39:45 +0800

    [OMNI-5169] Adjusting module version condition

--- src/Replication/Setup/UpgradeData.php
+++ src/Replication/Setup/UpgradeData.php
@@ -180,9 +180,8 @@ class UpgradeData implements UpgradeDataInterface
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        if (version_compare($context->getVersion(), '1.3.0', '<') ||
-            version_compare($context->getVersion(), '1.3.1', '<')) {
-            // only need to run  for existing clients who are using older version then 1.3.0
+        if (version_compare($context->getVersion(), '1.3.1', '<')) {
+            // only need to run  for existing clients who are using older version then 1.3.1
             $this->updateFlatTables();
             $this->updateConfigTable();
         }
