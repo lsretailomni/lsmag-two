48eaf18ff6fe3a3188c05606ec6a56114e9d2d0d
Umar Yousaf <umar.yousaf@briteskies.com>
2020-04-23 10:57:38 +0800

    [OMNI-5169] Updated the version and update data

--- src/Replication/Setup/UpgradeData.php
+++ src/Replication/Setup/UpgradeData.php
@@ -180,7 +180,7 @@ class UpgradeData implements UpgradeDataInterface
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        if (version_compare($context->getVersion(), '1.3.1', '<')) {
+        if (version_compare($context->getVersion(), '1.3.0', '<')) {
             // only need to run  for existing clients who are using older version then 1.3.1
             $this->updateFlatTables();
             $this->updateConfigTable();
