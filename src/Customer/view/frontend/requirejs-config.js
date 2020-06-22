4b6284eb797592128f7b82dd0afb48675b9d0842
Umar Yousaf <umar.yousaf@briteskies.com>
2020-06-22 16:32:17 +0800

    [OMNI-5229] Added datatables library plus used it for order history

--- src/Customer/view/frontend/requirejs-config.js
+++ src/Customer/view/frontend/requirejs-config.js
@@ -3,5 +3,8 @@ var config = {
         '*': {
             'Magento_Checkout/template/authentication.html': 'Ls_Customer/template/authentication.html'
         }
+    },
+    shim: {
+        'Ls_Core/js/datatables.min': ['jquery', 'jquery/ui']
     }
 };
