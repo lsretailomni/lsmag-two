d449f1ff64743ca0a2b90cbe1781f0692b517d57
Umar Yousaf <umar.yousaf@briteskies.com>
2020-07-07 02:17:48 +0800

    [OMNI-5209][OMNI-5206] Assignment data even if omni is down before order placement

--- src/Omni/Observer/DataAssignObserver.php
+++ src/Omni/Observer/DataAssignObserver.php
@@ -2,7 +2,6 @@
 
 namespace Ls\Omni\Observer;
 
-use \Ls\Core\Model\LSR;
 use Magento\Checkout\Model\Session\Proxy;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
@@ -13,11 +12,6 @@ use Magento\Framework\Event\ObserverInterface;
  */
 class DataAssignObserver implements ObserverInterface
 {
-
-    /** @var LSR @var */
-    private $lsr;
-
-
     /**
      * @var Proxy
      */
@@ -26,14 +20,11 @@ class DataAssignObserver implements ObserverInterface
 
     /**
      * DataAssignObserver constructor.
-     * @param LSR $LSR
      * @param Proxy $checkoutSession
      */
     public function __construct(
-        LSR $LSR,
         Proxy $checkoutSession
     ) {
-        $this->lsr             = $LSR;
         $this->checkoutSession = $checkoutSession;
     }
 
@@ -43,26 +34,21 @@ class DataAssignObserver implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        /*
-         * Adding condition to only process if LSR is enabled.
-         */
-        if ($this->lsr->isLSR($this->lsr->getCurrentStoreId())) {
-            $quote = $observer->getQuote();
-            $order = $observer->getOrder();
+        $quote = $observer->getQuote();
+        $order = $observer->getOrder();
 
-            $order->setPickupDate($quote->getPickupDate());
-            if ($quote->getPickupStore()) {
-                $order->setPickupStore($quote->getPickupStore());
-            }
-            if (!empty($this->checkoutSession->getCouponCode())) {
-                $order->setCouponCode($this->checkoutSession->getCouponCode());
-            }
-            $order->setLsPointsSpent($quote->getLsPointsSpent());
-            $order->setLsPointsEarn($quote->getLsPointsEarn());
-
-            $order->setLsGiftCardAmountUsed($quote->getLsGiftCardAmountUsed());
-            $order->setLsGiftCardNo($quote->getLsGiftCardNo());
+        $order->setPickupDate($quote->getPickupDate());
+        if ($quote->getPickupStore()) {
+            $order->setPickupStore($quote->getPickupStore());
         }
+        if (!empty($this->checkoutSession->getCouponCode())) {
+            $order->setCouponCode($this->checkoutSession->getCouponCode());
+        }
+        $order->setLsPointsSpent($quote->getLsPointsSpent());
+        $order->setLsPointsEarn($quote->getLsPointsEarn());
+
+        $order->setLsGiftCardAmountUsed($quote->getLsGiftCardAmountUsed());
+        $order->setLsGiftCardNo($quote->getLsGiftCardNo());
         return $this;
     }
 }
