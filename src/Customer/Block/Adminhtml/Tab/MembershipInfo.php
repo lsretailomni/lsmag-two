5c859f37e5de114b679cd89e5284ccc318ffae41
Sudhanshu Bajaj <sudhanshu.b@lsretail.com>
2020-07-13 17:40:16 +0800

    [OMNI-5136] Membership information tab on customer edit page

---
+++ src/Customer/Block/Adminhtml/Tab/MembershipInfo.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Ls\Customer\Block\Adminhtml\Tab;
+
+use Magento\Backend\Block\Template;
+use Magento\Framework\Phrase;
+use Magento\Backend\Block\Template\Context;
+use Magento\Ui\Component\Layout\Tabs\TabInterface;
+use Magento\Customer\Model\Customer;
+
+/**
+ * Customer membership view block
+ * Class MembershipInfo
+ * @package Ls\Customer\Block\Adminhtml\Tab
+ */
+class MembershipInfo extends Template implements TabInterface
+{
+
+    /**
+     * @var Customer
+     */
+    protected $customerObject;
+
+    /**
+     * MembershipInfo constructor.
+     * @param Context $context
+     * @param Customer $customerObject
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Customer $customerObject,
+        array $data = []
+    ) {
+        $this->customerObject = $customerObject;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return Phrase
+     */
+    public function getTabLabel()
+    {
+        return __('LS Central Membership');
+    }
+
+    /**
+     * @return Phrase
+     */
+    public function getTabTitle()
+    {
+        return __('LS Central Membership');
+    }
+
+    /**
+     * @return bool
+     */
+    public function canShowTab()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHidden()
+    {
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTabClass()
+    {
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getTabUrl()
+    {
+        return '';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAjaxLoaded()
+    {
+        return false;
+    }
+
+    /**
+     * Returns customer's membership details
+     *
+     * @return array
+     */
+    public function getMembershipInfo()
+    {
+        try {
+            $data            = $this->_backendSession->getCustomerData();
+            $customerDetails = $this->customerObject->loadByEmail($data['account']['email']);
+            return [
+                'lsr_id'       => $customerDetails->getData('lsr_id'),
+                'lsr_cardid'   => $customerDetails->getData('lsr_cardid'),
+                'lsr_username' => $customerDetails->getData('lsr_username')
+            ];
+        } catch (\Exception $e) {
+            $this->_logger->critical($e);
+            return [];
+        }
+    }
+}
