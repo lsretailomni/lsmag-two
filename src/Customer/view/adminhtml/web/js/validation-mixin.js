c2bb39fd854105e693ed3aa62cd17b8d2054825c
Umar Yousaf <umarusaf902@gmail.com>
2020-09-15 13:54:55 +0800

    [OMNI-5272] updated the prefix validation label

--- src/Customer/view/adminhtml/web/js/validation-mixin.js
+++ src/Customer/view/adminhtml/web/js/validation-mixin.js
@@ -7,7 +7,7 @@ define(['jquery'], function ($) {
             function (value) {
                 return value.match(/^[a-zA-Z0-9-_@.]+$/);
             },
-            $.mage.__('Enter a valid username. Valid characters are A-Z a-z 0-9 . _ - @.')
+            $.mage.__('Enter a valid username prefix. Valid characters are A-Z a-z 0-9 . _ - @.')
         );
     }
 });
