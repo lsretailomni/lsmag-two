83c37e931b4f957f9edbff7b68faa06de88df09a
Zeeshan Khuwaja <zeeshan.khuwaja@lsretail.com>
2020-10-27 14:24:43 +0800

    [OMNI-5257] fix replication after omni update

--- src/Replication/Cron/AbstractReplicationTask.php
+++ src/Replication/Cron/AbstractReplicationTask.php
@@ -123,10 +123,10 @@ abstract class AbstractReplicationTask
         "ls_mag/replication/repl_store_tender_type"          => ["StoreID", "TenderTypeId", "scope_id"],
         "ls_mag/replication/repl_unit_of_measure"            => ["nav_id", "scope_id"],
         "ls_mag/replication/repl_vendor"                     => ["Name", "scope_id"],
-        "ls_mag/replication/repl_hierarchy_hosp_deal_line"   => ["DealNo", "ItemNo", "UnitOfMeasure", "scope_id"],
-        "ls_mag/replication/repl_hierarchy_hosp_deal"        => ["DealNo", "ItemNo", "UnitOfMeasure", "scope_id"],
+        "ls_mag/replication/repl_hierarchy_hosp_deal_line"   => ["DealNo", "ItemNo","LineNo", "UnitOfMeasure", "scope_id"],
+        "ls_mag/replication/repl_hierarchy_hosp_deal"        => ["DealNo", "No", "LineNo","UnitOfMeasure", "scope_id"],
         "ls_mag/replication/repl_hierarchy_hosp_recipe"      => ["ItemNo", "RecipeNo", "UnitOfMeasure", "scope_id"],
-        "ls_mag/replication/repl_hierarchy_hosp_modifier"    => ["ItemNo", "Code", "SubCode", "UnitOfMeasure", "scope_id"],
+        "ls_mag/replication/repl_hierarchy_hosp_modifier"    => ["ItemNo","ParentItem", "Code", "SubCode", "UnitOfMeasure", "scope_id"],
         "ls_mag/replication/loy_item"                        => ["nav_id", "scope_id"]
     ];
 
