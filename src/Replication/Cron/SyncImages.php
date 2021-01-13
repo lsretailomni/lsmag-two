040cd0be130947555b770128a612e694357f13b4
Umar Yousaf <umarusaf902@gmail.com>
2021-01-13 17:59:02 +0800

    [OMNI-5350] Item images were not showing on PLP

--- src/Replication/Cron/SyncImages.php
+++ src/Replication/Cron/SyncImages.php
@@ -207,7 +207,7 @@ class SyncImages extends ProductCreateTask
             $itemSku = $itemSku . '-' . $uomCode;
         }
         try {
-            $productData = $this->productRepository->get($itemSku, true, 0, true);
+            $productData = $this->productRepository->get($itemSku, true, null, true);
         } catch (NoSuchEntityException $e) {
             return;
         }
