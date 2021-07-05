e362e0a396a521e61b592fe7468db8db7f65ddc1
Umar Yousaf <umarusaf902@gmail.com>
2021-07-05 15:02:08 +0800

    [OMNI-5480] Updating basket and wishlist after login in graphql

--- src/CustomerGraphQl/Plugin/AccountManagement.php
+++ src/CustomerGraphQl/Plugin/AccountManagement.php
@@ -128,6 +128,7 @@ class AccountManagement
                     $login['username'] = $username;
                     $login['password'] = $password;
                     $this->contactHelper->processCustomerLogin($result, $login, $isEmail);
+                    $this->contactHelper->updateBasketAndWishlistAfterLogin($result);
                     $email = $result->getEmail();
                 }
             } else {
