76bda58ead04e6b72e0892417c01e15f16a6da4f
deepak <deepakkna@lsretail.com>
2022-04-27 10:06:48 +0530

    CS fixes

--- src/Omni/Plugin/Order/TrackPlugin.php
+++ src/Omni/Plugin/Order/TrackPlugin.php
@@ -2,7 +2,7 @@
 
 namespace Ls\Omni\Plugin\Order;
 
-use Magento\Framework\Api\AttributeValueFactory;
+use Magento\Shipping\Model\Order\Track;
 
 /**
  * Plugin to intercept Shipment Track details load
@@ -10,25 +10,17 @@ use Magento\Framework\Api\AttributeValueFactory;
  */
 class TrackPlugin extends \Magento\Sales\Model\Order\Shipment\Track
 {
-    public function __construct(
-        \Magento\Shipping\Model\CarrierFactory $carrierFactory,
-        \Magento\Sales\Model\Order\Shipment\Track $trackShipment
-    ) {
-        $this->_carrierFactory = $carrierFactory;
-        $this->trackShipment = $trackShipment;
-    }
     /**
      * Retrieve detail for shipment track with ls central shipping id
-     *
-     * @return \Magento\Framework\Phrase|string
+     * @param Track $subject
+     * @param $proceed
+     * @return mixed
      */
     public function afterGetNumberDetail(
-        \Magento\Shipping\Model\Order\Track $subject,
+        Track $subject,
         $proceed
-    )
-    {
-
-        if(is_array($proceed)) {
+    ) {
+        if (is_array($proceed)) {
             $proceed['ls_central_shipping_id'] = $subject->getLsCentralShippingId();
         }
         return $proceed;
