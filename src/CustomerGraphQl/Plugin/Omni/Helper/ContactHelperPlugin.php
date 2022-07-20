b23a549cb4ff50de30e3c2a28dc1b50559b51702
Umar Yousaf <umaryo@lsretail.com>
2022-07-20 18:56:30 +0800

    [23235] Fixed crash on pwa checkout due to incorrect merging of quotes

---
+++ src/CustomerGraphQl/Plugin/Omni/Helper/ContactHelperPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ls\CustomerGraphQl\Plugin\Omni\Helper;
+
+use \Ls\Omni\Client\Ecommerce\Entity\MemberContact;
+use \Ls\Omni\Helper\ContactHelper;
+
+/**
+ * Interceptor to intercept methods in ContactHelper
+ */
+class ContactHelperPlugin
+{
+    /**
+     * Before plugin to clear existing quote in the checkout session
+     *
+     * To fix issue with merging quotes causing crash on checkout
+     *
+     * @param ContactHelper $subject
+     * @param MemberContact $result
+     * @param array $credentials
+     * @param string $is_email
+     * @return array
+     */
+    public function beforeProcessCustomerLogin(
+        ContactHelper $subject,
+        MemberContact $result,
+        $credentials,
+        $is_email
+    ) {
+        $subject->checkoutSession->clearQuote();
+
+        return [$result, $credentials, $is_email];
+    }
+}
