f4f4d474e1b7bf3826f567e6bfe05ba228dd5938
Umar Yousaf <umaryo@lsretail.com>
2022-09-22 16:28:59 +0800

    [#26801] Stop loading ko components on checkout and cart based on admin configuration + added conditions inside mixins if required

--- src/Omni/Plugin/Cart/Model/CartTotalsProcessorPlugin.php
+++ src/Omni/Plugin/Cart/Model/CartTotalsProcessorPlugin.php
@@ -2,42 +2,58 @@
 
 namespace Ls\Omni\Plugin\Cart\Model;
 
+use \Ls\Core\Model\LSR;
 use \Ls\Omni\Helper\LoyaltyHelper;
 use Magento\Checkout\Block\Cart\CartTotalsProcessor;
+use Magento\Framework\Exception\NoSuchEntityException;
 
-/**
- * Class CartTotalsProcessorPlugin
- * @package Ls\Omni\Plugin\Cart\Model
- */
 class CartTotalsProcessorPlugin
 {
+    /**
+     * @var LSR
+     */
+    public $lsr;
+
     /**
      * @var LoyaltyHelper
      */
     public $loyaltyHelper;
 
     /**
-     * CartTotalsProcessorPlugin constructor.
      * @param LoyaltyHelper $loyaltyHelper
+     * @param LSR $lsr
      */
     public function __construct(
-        LoyaltyHelper $loyaltyHelper
+        LoyaltyHelper $loyaltyHelper,
+        LSR $lsr
     ) {
         $this->loyaltyHelper = $loyaltyHelper;
+        $this->lsr           = $lsr;
     }
 
     /**
+     * After plugin to update the layout processor of cart page
+     *
      * @param CartTotalsProcessor $subject
      * @param array $jsLayout
      * @return array
+     * @throws NoSuchEntityException
      */
     public function afterProcess(
         CartTotalsProcessor $subject,
         array $jsLayout
     ) {
+        if (!$this->lsr->isEnabled()) {
+            $totalsBlock = &$jsLayout['components']['block-totals'];
+            unset($totalsBlock['children']['loyalty_points_label']);
+            unset($totalsBlock['children']['before_grandtotal']['children']['ls_points_discount']);
+            unset($totalsBlock['children']['before_grandtotal']['children']['ls_gift_card_amount_used']);
+        }
+
         if ($this->loyaltyHelper->isLoyaltyPointsEnabled('cart') == '0') {
             unset($jsLayout['components']['block-totals']['children']['loyalty_points_label']);
         }
+
         return $jsLayout;
     }
 }
--- src/Omni/view/frontend/web/js/model/shipping-save-processor/default.js
+++ src/Omni/view/frontend/web/js/model/shipping-save-processor/default.js
@@ -7,10 +7,14 @@
 
     return function (payloadExtender) {
         return wrapper.wrap(payloadExtender, function (originalFunction, payload) {
-            let pickupDate = $('[name="pickup-date"]') ? $('[name="pickup-date"]').val() : '';
-            let pickupTimeslot = $('[name="pickup-timeslot"]') ? $('[name="pickup-timeslot"]').val() : '';
             payload = originalFunction(payload);
 
+            if (!window.checkoutConfig.ls_enabled) {
+                return payload;
+            }
+
+            let pickupDate = $('[name="pickup-date"]') ? $('[name="pickup-date"]').val() : '';
+            let pickupTimeslot = $('[name="pickup-timeslot"]') ? $('[name="pickup-timeslot"]').val() : '';
             _.extend(payload.addressInformation, {
                 extension_attributes: {
                     'pickup_store': $('#pickup-store').val(),
