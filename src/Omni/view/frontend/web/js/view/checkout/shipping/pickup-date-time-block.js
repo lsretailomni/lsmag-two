6625aad8317ee48fb81a5f814fce8b4ca79f6881
Umar Yousaf <umaryo@lsretail.com>
2022-09-29 11:45:15 +0800

    [#21353] Fixed validation for requested delivery date on hospitality

--- src/Omni/view/frontend/web/js/view/checkout/shipping/pickup-date-time-block.js
+++ src/Omni/view/frontend/web/js/view/checkout/shipping/pickup-date-time-block.js
@@ -1,9 +1,10 @@
 define([
+    'jquery',
     'uiComponent',
     'ko',
-    'Magento_Checkout/js/model/quote'
-
-], function (Component, ko, quote) {
+    'Magento_Checkout/js/model/quote',
+    'mage/translate',
+], function ($, Component, ko, quote, $t) {
     'use strict';
 
     return Component.extend({
@@ -17,6 +18,8 @@ define([
                 if (method && method['carrier_code'] !== undefined) {
                     if (method['carrier_code'] === 'clickandcollect' &&
                         window.checkoutConfig.shipping.pickup_date_timeslots.store_type === 1) {
+                        $("[name='shippingOptionSelect.pickup-timeslot']").show();
+                        $("[name='shippingOptionSelect.pickup-date'] .label span").text($t('Pickup Date'));
                         return true;
                     }
                     if (window.checkoutConfig.shipping.pickup_date_timeslots.store_type === 0
@@ -24,10 +27,27 @@ define([
                     ) {
                         return true;
                     }
+
+                    if (method['carrier_code'] !== 'clickandcollect' &&
+                        window.checkoutConfig.shipping.pickup_date_timeslots.store_type === 1
+                    ) {
+                        $("[name='shippingOptionSelect.pickup-timeslot']").hide();
+                        $("[name='shippingOptionSelect.pickup-date'] .label span").text($t('Requested Delivery Date'));
+                        return true;
+                    }
                 }
                 return false;
             }, this);
             return this;
+        },
+        renderedHandler: function () {
+            var method = quote.shippingMethod();
+            if (method && method['carrier_code'] !== undefined) {
+                if (method['carrier_code'] !== 'clickandcollect') {
+                    $("[name='shippingOptionSelect.pickup-timeslot']").hide();
+                    $("[name='shippingOptionSelect.pickup-date'] .label span").text($t('Requested Delivery Date'));
+                }
+            }
         },
         isDisplay: function () {
             return window.checkoutConfig.shipping.pickup_date_timeslots.enabled === "1";
