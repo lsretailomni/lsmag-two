fd4d34a4239750fd953c70570c5a2a1d9cfad582
Deepak K <deepakkna@lsretail.com>
2022-11-08 09:55:09 +0800

    [#28105] Removing code merge from OMNI-5080

--- src/Replication/Cron/SyncImages.php
+++ src/Replication/Cron/SyncImages.php
@@ -458,11 +458,10 @@ class SyncImages extends ProductCreateTask
     public function convertToRequiredFormat(array $mediaGalleryEntries)
     {
         $images = [];
-        foreach ($mediaGalleryEntries as $key => $entry) {
-            $images[$key] = $this->entryConverterPool
+        foreach ($mediaGalleryEntries as $entry) {
+            $images[] = $this->entryConverterPool
                 ->getConverterByMediaType($entry->getMediaType())
                 ->convertFrom($entry);
-            $images[$key]['image_id'] = $entry->getData('image_id');
         }
         return $images;
     }
