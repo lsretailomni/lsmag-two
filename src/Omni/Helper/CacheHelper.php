12550f34696eb83683e122acf50904c6ac86ae0c
Syed Waqas Nabi <syedna@lsretail.com>
2023-01-05 16:07:03 +0800

    [#31266] Removed unused code

--- src/Omni/Helper/CacheHelper.php
+++ src/Omni/Helper/CacheHelper.php
@@ -26,8 +26,7 @@ class CacheHelper extends AbstractHelper
     public $soapOptions = [
         'cache_wsdl'   => WSDL_CACHE_DISK,
         'soap_version' => SOAP_1_1,
-        'features'     => SOAP_SINGLE_ELEMENT_ARRAYS,
-        'encoding'     => 'UTF-8'
+        'features'     => SOAP_SINGLE_ELEMENT_ARRAYS
     ];
 
     /**
