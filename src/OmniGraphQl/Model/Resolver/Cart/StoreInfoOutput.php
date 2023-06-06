662e2899f4c1461aaa9e90a21b1cd99881c4f9a3
Syed Waqas Nabi <syedna@lsretail.com>
2023-06-06 20:42:53 +0800

    [#38477] Fixed graphql selected shipping method checkout session issue for guest user

--- src/OmniGraphQl/Model/Resolver/Cart/StoreInfoOutput.php
+++ src/OmniGraphQl/Model/Resolver/Cart/StoreInfoOutput.php
@@ -10,7 +10,6 @@ use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Zend_Log_Exception;
 
 class StoreInfoOutput implements ResolverInterface
 {
@@ -19,19 +18,17 @@ class StoreInfoOutput implements ResolverInterface
      */
     public $dataHelper;
 
-    /**
-     * @param DataHelper $dataHelper
-     */
+
     public function __construct(
         DataHelper $dataHelper,
         CheckoutSession $checkoutSession
     ) {
-        $this->dataHelper       = $dataHelper;
-        $this->checkoutSession  = $checkoutSession;
+        $this->dataHelper      = $dataHelper;
+        $this->checkoutSession = $checkoutSession;
     }
 
     /**
-     * Add proper swatch image path
+     * Store information
      *
      * @param Field $field
      * @param ContextInterface $context
@@ -41,7 +38,7 @@ class StoreInfoOutput implements ResolverInterface
      *
      * @return array
      *
-     * @throws NoSuchEntityException|Zend_Log_Exception
+     * @throws NoSuchEntityException
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
@@ -52,32 +49,33 @@ class StoreInfoOutput implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
-
         $storeInfo = [];
-        $cart   = $this->checkoutSession->getQuote();
-        $pickupStoreId = $cart->getPickupStore();
-        $pickupStoreName = ($pickupStoreId) ? $this->dataHelper->getStoreNameById($pickupStoreId) : "";
+        if (isset($value['cart']) && isset($value['cart']['model'])) {
+            $cart            = $value['cart']['model'];
+            $pickupStoreId   = $cart->getPickupStore();
+            $pickupStoreName = ($pickupStoreId) ? $this->dataHelper->getStoreNameById($pickupStoreId) : "";
 
-        if ($pickupStoreId && $cart->getShippingAddress()->getShippingMethod() == "clickandcollect_clickandcollect") {
+            if ($pickupStoreId && $cart->getShippingAddress()->getShippingMethod() == "clickandcollect_clickandcollect") {
 
-            $pickupDate = $pickupTime = "";
-            if ($cart->getPickupDateTimeslot()
-                && count($dateTimeArr = explode(" ", $cart->getPickupDateTimeslot()))>0
-            ) {
-                $pickupDate  = $dateTimeArr[0];
-                if (array_key_exists("1", $dateTimeArr)) {
-                    $pickupTime  = array_key_exists("2", $dateTimeArr) ?
-                        $dateTimeArr["1"]." ".$dateTimeArr["2"]
-                        : $dateTimeArr[1];
+                $pickupDate = $pickupTime = "";
+                if ($cart->getPickupDateTimeslot()
+                    && count($dateTimeArr = explode(" ", $cart->getPickupDateTimeslot())) > 0
+                ) {
+                    $pickupDate = $dateTimeArr[0];
+                    if (array_key_exists("1", $dateTimeArr)) {
+                        $pickupTime = array_key_exists("2", $dateTimeArr) ?
+                            $dateTimeArr["1"] . " " . $dateTimeArr["2"]
+                            : $dateTimeArr[1];
+                    }
+
                 }
 
+                $storeInfo["store_id"]    = $pickupStoreId;
+                $storeInfo["store_name"]  = $pickupStoreName;
+                $storeInfo["pickup_date"] = $pickupDate;
+                $storeInfo["pickup_time"] = $pickupTime;
+
             }
-
-            $storeInfo["store_id"] = $pickupStoreId;
-            $storeInfo["store_name"] = $pickupStoreName;
-            $storeInfo["pickup_date"] = $pickupDate;
-            $storeInfo["pickup_time"] = $pickupTime;
-
         }
 
         return $storeInfo;
