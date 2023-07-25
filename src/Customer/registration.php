46ed14a9d2e403df4c972f767d1416939b6d28fb
Umar Yousaf <umaryo@lsretail.com>
2023-07-25 12:51:18 +0800

    [#40132] Fixed error with pipeline

--- src/Customer/registration.php
+++ src/Customer/registration.php
@@ -5,5 +5,5 @@ use Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'Ls_Customer',
-    isset($file) ? $file->dirname($file) : __DIR__
+    isset($file) ? dirname($file) : __DIR__
 );
