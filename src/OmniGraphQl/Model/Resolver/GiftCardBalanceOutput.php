ead5f83bb00b6aa9f5943b659ac2172e6e350b64
Syed Waqas Nabi <syedna@lsretail.com>
2023-10-31 13:32:48 +0800

    [#44319] GraphQl - Get Stores and Get Gift Card Balance

---
+++ src/OmniGraphQl/Model/Resolver/GiftCardBalanceOutput.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ls\OmniGraphQl\Model\Resolver;
+
+use \Ls\Omni\Helper\GiftCardHelper;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+/**
+ * To return gift card balance in graphql
+ */
+class GiftCardBalanceOutput implements ResolverInterface
+{
+    /**
+     * @var GiftCardHelper
+     */
+    public $giftCardHelper;
+
+    /**
+     * @param GiftCardHelper $giftCardHelper
+     */
+    public function __construct(
+        GiftCardHelper $giftCardHelper
+    ) {
+        $this->giftCardHelper = $giftCardHelper;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (empty($args['gift_card_no'])) {
+            throw new GraphQlInputException(__('Required parameter "gift_card_no" is missing'));
+        }
+
+        $giftCardPin     = $args['gift_card_pin'];
+        $giftCardBalance = '';
+        $currency        = '';
+
+        $response = $this->giftCardHelper->getGiftCardBalance($args['gift_card_no'], $giftCardPin);
+        if ($response) {
+            $giftCardBalance = $response->getBalance();
+            $currency        = $response->getCurrencyCode();
+        }
+
+        return [
+            'currency' => $currency,
+            'value'    => $giftCardBalance,
+        ];
+    }
+}
