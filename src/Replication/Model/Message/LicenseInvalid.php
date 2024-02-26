e6c9674ab5ddc2672461c6c7ba2f040480d681a6
Umar Yousaf <umaryo@lsretail.com>
2024-02-26 13:04:54 +0500

    [#49158] Added flag in the configuration to show the license validity

--- src/Replication/Model/Message/LicenseInvalid.php
+++ src/Replication/Model/Message/LicenseInvalid.php
@@ -62,7 +62,7 @@ class LicenseInvalid implements MessageInterface
                 $str = $this->lsr->getCentralVersion($store->getWebsiteId(), ScopeInterface::SCOPE_WEBSITES);
                 $centralVersion =  strstr($str, " ", true);
 
-                if (version_compare($centralVersion, '24.0.0.0', '>=') &&
+                if (version_compare($centralVersion, '25.0.0.0', '>=') &&
                     $this->lsr->getWebsiteConfig(
                         LSR::SC_SERVICE_LICENSE_VALIDITY,
                         $store->getWebsiteId()
