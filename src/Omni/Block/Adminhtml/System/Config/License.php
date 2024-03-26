68dd21d73e769d068907dfce5a1fbeaba22cc00d
Syed Waqas Nabi <s.nabi@roobeo.io>
2024-03-26 16:40:03 +0800

    [#50849] Fixed license message during fresh installation of Magento

--- src/Omni/Block/Adminhtml/System/Config/License.php
+++ src/Omni/Block/Adminhtml/System/Config/License.php
@@ -43,10 +43,12 @@ class License extends Field
         $websiteId = $element->getScopeId();
 
         $str = $this->lsr->getCentralVersion($websiteId, ScopeInterface::SCOPE_WEBSITES);
-        $centralVersion =  strstr($str, " ", true);
+        if ($str) {
+            $centralVersion = strstr($str, " ", true);
 
-        if (version_compare($centralVersion, '24.0.0.0', '<') || $element->getValue() === null) {
-            return '';
+            if (version_compare($centralVersion, '24.0.0.0', '<') || $element->getValue() === null) {
+                return '';
+            }
         }
 
         return parent::render($element);
@@ -60,13 +62,13 @@ class License extends Field
      */
     protected function _getElementHtml(AbstractElement $element)
     {
-        $validClass = 'valid-license';
+        $validClass   = 'valid-license';
         $invalidClass = 'invalid-license';
-        $html = "<div class='control-value ";
-        $html .= $element->getValue() == "1" ? $validClass : $invalidClass;
-        $html .= "'>";
-        $html .= $element->getValue() == "1" ? __('Valid') : __('Invalid');
-        $html .= "</div>";
+        $html         = "<div class='control-value ";
+        $html         .= $element->getValue() == "1" ? $validClass : $invalidClass;
+        $html         .= "'>";
+        $html         .= $element->getValue() == "1" ? __('Valid') : __('Invalid');
+        $html         .= "</div>";
 
         return $html;
     }
