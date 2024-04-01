4a6c35ece9809550aed834e690705a314325cc8d
Syed Waqas Nabi <syedna@lsretail.com>
2024-04-01 08:13:43 +0800

    [#48012] Fixed order edit test case

--- src/Omni/Test/Unit/Client/Ecommerce/Operation/OrderEditMethodsTest.php
+++ src/Omni/Test/Unit/Client/Ecommerce/Operation/OrderEditMethodsTest.php
@@ -2,98 +2,94 @@
 
 namespace Ls\Omni\Test\Unit\Client\Ecommerce\Operation;
 
-use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfOneListItem;
+use Ls\Omni\Client\Ecommerce\Entity\ArrayOfOrderLine;
 use \Ls\Omni\Client\Ecommerce\Entity\ArrayOfOrderPayment;
-use \Ls\Omni\Client\Ecommerce\Entity\Enum\ListType;
-use \Ls\Omni\Client\Ecommerce\Entity\Enum\OrderType;
-use \Ls\Omni\Client\Ecommerce\Entity\OneList;
-use \Ls\Omni\Client\Ecommerce\Entity\OneListCalculate;
-use \Ls\Omni\Client\Ecommerce\Entity\OneListItem;
-use \Ls\Omni\Client\Ecommerce\Entity\Order;
-use \Ls\Omni\Client\Ecommerce\Entity\OrderLine;
+use Ls\Omni\Client\Ecommerce\Entity\Enum\LineType;
+use Ls\Omni\Client\Ecommerce\Entity\Enum\OrderEditType;
+use Ls\Omni\Client\Ecommerce\Entity\OrderLine;
 use \Ls\Omni\Client\Ecommerce\Entity\OrderPayment;
 use \Ls\Omni\Client\Ecommerce\Entity\SalesEntry;
-use \Ls\Omni\Exception\InvalidEnumException;
+use \Ls\Omni\Client\Ecommerce\Entity\Order as CommerceOrder;
+use \Ls\Omni\Client\Ecommerce\Entity\Address;
 
 /**
- * It will cover all the methods used for Order Creation Cycle
+ * It will cover test for order edit
  */
 class OrderEditMethodsTest extends OmniClientSetupTest
 {
     /**
-     * Get One List
-     *
-     * @return mixed
-     * @throws InvalidEnumException
-     */
-    public function getOneList($cardId = '')
-    {
-        $listItems = new OneListItem();
-        $listItems
-            ->setItemId($this->getEnvironmentVariableValueGivenName('ITEM_ID'))
-            ->setVariantId($this->getEnvironmentVariableValueGivenName('VARIANT_ID'))
-            ->setQuantity(1);
-        $itemsArray = new ArrayOfOneListItem();
-        $itemsArray->setOneListItem($listItems);
-        $oneListRequest = new OneList();
-        $oneListRequest
-            ->setItems($itemsArray)
-            ->setCardId($cardId)
-            ->setStoreId($this->getEnvironmentVariableValueGivenName('STORE_ID'))
-            ->setListType(ListType::BASKET);
-        $param = [
-            'oneList' => $oneListRequest,
-            'calculate' => true
-        ];
-
-        return $this->client->OneListSave($param);
-    }
-
-
-
-    /**
      * Edit customer order
-     * Type - ClickAndCollect
+     * Type - Order Edit
      * User - Member
      * PaymentLine - Cash
      * @depends testOneListSaveBasket
      */
     public function testOrderEdit()
     {
-        $response       = $this->getOneList($this->getEnvironmentVariableValueGivenName('CARD_ID'));
-        $oneListRequest = $response->getResult();
-        $entity         = new OneListCalculate();
-        $entity->setOneList($oneListRequest);
-        $response = $this->client->OneListCalculate($entity);
-        $result   = $response->getResult();
-        $this->assertInstanceOf(Order::class, $result);
+        $orderObject = new CommerceOrder();
+        $orderObject->setStoreId($this->getEnvironmentVariableValueGivenName('STORE_ID'));
+        $orderObject->setCardId($this->getEnvironmentVariableValueGivenName('CARD_ID'));
+        $orderObject->setEmail($this->getEnvironmentVariableValueGivenName('EMAIL'));
         $orderPayment = new OrderPayment();
         $orderPayment->setCurrencyFactor(1)
-            ->setAmount($result->getTotalAmount())
-            ->setLineNumber('1')
+            ->setAmount(100)
+            ->setLineNumber('20')
             ->setExternalReference('TEST0012345')
             ->setTenderType($this->getEnvironmentVariableValueGivenName('CASH_TENDER_TYPE'));
         $orderPayments = new ArrayOfOrderPayment();
         $orderPayments->setOrderPayment([$orderPayment]);
-        $result->setOrderPayments($orderPayments);
-        $result->setOrderType(OrderType::CLICK_AND_COLLECT);
-        $result->setId('test' . substr(preg_replace("/[^A-Za-z0-9 ]/", '', $result->getId()), 0, 10));
-        // Order creation request
-        $paramOrderCreate  = [
-            'request' => $result
+        $orderObject->setOrderPayments($orderPayments);
+        $commerceAddress = new Address();
+        $commerceAddress->setCity('KL')
+            ->setAddress1('Jalan')
+            ->setAddress2('Klang')
+            ->setCountry('MY')
+            ->setStateProvinceRegion('Kuala Lumpur')
+            ->setPostCode('47301');
+        $orderObject
+            ->setId('TEST0012345')
+            ->setCardId($this->getEnvironmentVariableValueGivenName('CARD_ID'))
+            ->setEmail($this->getEnvironmentVariableValueGivenName('EMAIL'))
+            ->setShipToEmail($this->getEnvironmentVariableValueGivenName('EMAIL'))
+            ->setContactName('test')
+            ->setShipToName('test')
+            ->setContactAddress($commerceAddress)
+            ->setShipToAddress($commerceAddress)
+            ->setStoreId($this->getEnvironmentVariableValueGivenName('STORE_ID'));
+
+        $lineOrder = new OrderLine();
+        $lineOrder->setPrice(60)
+            ->setAmount(60)
+            ->setNetPrice(40)
+            ->setNetAmount(40)
+            ->setTaxAmount(20)
+            ->setItemId($this->getEnvironmentVariableValueGivenName('ITEM_ID'))
+            ->setVariantId($this->getEnvironmentVariableValueGivenName('VARIANT_ID'))
+            ->setLineType(LineType::ITEM)
+            ->setLineNumber(100)
+            ->setQuantity(2);
+
+        $orderLinesArray = new ArrayOfOrderLine();
+        $orderLinesArray->setOrderLine([$lineOrder]);
+        $orderObject->setOrderLines($orderLinesArray);
+        // Order edit request
+        $paramEditCreate  = [
+            'request' => $orderObject,
+            'editType' => OrderEditType::GENERAL,
+            'orderId' => $this->getEnvironmentVariableValueGivenName('DOCUMENT_ID')
         ];
-        $responseOrder     = $this->client->OrderEdit($paramOrderCreate);
-        $resultOrderCreate = $responseOrder->getResult();
-        $this->assertInstanceOf(SalesEntry::class, $resultOrderCreate);
-        $this->assertTrue(property_exists($resultOrderCreate, 'Id'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'CardId'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'ExternalId'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'StoreId'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'TotalAmount'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'TotalDiscount'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'TotalNetAmount'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'Status'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'Payments'));
-        $this->assertTrue(property_exists($resultOrderCreate, 'Lines'));
+        $responseOrder     = $this->client->OrderEdit($paramEditCreate);
+        $resultOrderEdit = $responseOrder->getResult();
+        $this->assertInstanceOf(SalesEntry::class, $resultOrderEdit);
+        $this->assertTrue(property_exists($resultOrderEdit, 'Id'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'CardId'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'ExternalId'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'StoreId'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'TotalAmount'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'TotalDiscount'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'TotalNetAmount'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'Status'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'Payments'));
+        $this->assertTrue(property_exists($resultOrderEdit, 'Lines'));
     }
 }
