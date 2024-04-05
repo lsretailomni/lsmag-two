3642d753e3f761fd4e52c07dfadb893e36e3f2b9
Umar Yousaf <umaryo@lsretail.com>
2024-04-05 12:13:24 +0500

    [#51203] Passing correct scopeId to return click and collect stores data

--- src/OmniGraphQl/Model/Resolver/ClickAndCollectStoresOutput.php
+++ src/OmniGraphQl/Model/Resolver/ClickAndCollectStoresOutput.php
@@ -31,7 +31,7 @@ class ClickAndCollectStoresOutput implements ResolverInterface
      */
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
-        $scopeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+        $scopeId = (int)$context->getExtensionAttributes()->getStore()->getWebsiteId();
         $stores  = $this->dataHelper->getStores($scopeId);
         $stores  = $stores->toArray()['items'];
 
