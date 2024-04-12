7752d9827c2a695c7fb6f6a0dfe2a01006713ef9
Deepak K <deepakkna@lsretail.com>
2024-04-12 16:16:51 +0530

    51243 Fix for lsdiscount giving null response in graphql

--- src/OmniGraphQl/Plugin/Model/Resolver/CartPricesPlugin.php
+++ src/OmniGraphQl/Plugin/Model/Resolver/CartPricesPlugin.php
@@ -2,11 +2,13 @@
 
 namespace Ls\OmniGraphQl\Plugin\Model\Resolver;
 
+use Ls\Omni\Helper\BasketHelper;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Address\Total;
 use Magento\Quote\Model\Quote\TotalsCollector;
+use Magento\Tests\NamingConvention\true\object;
 
 /**
  * Interceptor to return vat value of the whole cart
@@ -18,14 +20,21 @@ class CartPricesPlugin
      * @var TotalsCollector
      */
     private $totalsCollector;
+    /**
+     * @var BasketHelper
+     */
+    private BasketHelper $basketHelper;
 
     /**
      * @param TotalsCollector $totalsCollector
+     * @param BasketHelper $basketHelper
      */
     public function __construct(
-        TotalsCollector $totalsCollector
+        TotalsCollector $totalsCollector,
+        BasketHelper $basketHelper,
     ) {
         $this->totalsCollector = $totalsCollector;
+        $this->basketHelper    = $basketHelper;
     }
 
     /**
@@ -55,7 +64,7 @@ class CartPricesPlugin
         $cartTotals           = $this->totalsCollector->collectQuoteTotals($quote);
         $currency             = $quote->getQuoteCurrencyCode();
         $result['lstax']      = $this->getVat($cartTotals, $currency);
-        $result['lsdiscount'] = $this->getDiscounts($cartTotals, $currency);
+        $result['lsdiscount'] = $this->getDiscounts($quote, $cartTotals, $currency);
 
         return $result;
     }
@@ -79,20 +88,38 @@ class CartPricesPlugin
     }
 
     /**
-     * Returns information related to discounts from total
+     *  Returns information related to discounts from total
      *
+     * @param object $quote
      * @param Total $total
      * @param string $currency
      * @return array|null
      */
-    private function getDiscounts(Total $total, string $currency)
+    private function getDiscounts($quote, Total $total, string $currency)
     {
-        if ($total->getDiscountAmount() === 0) {
+        $totalDiscounts = $this->getTotalDiscount($quote);
+        if ($totalDiscounts === 0) {
             return null;
         }
         return [
-            'amount' => ['value' => $total->getDiscountAmount(), 'currency' => $currency],
+            'amount' => ['value' => $totalDiscounts, 'currency' => $currency],
             'label'  => __('Discount')
         ];
+    }
+
+    /**
+     * Calculate total discounts in quote based on basket calculation response
+     *
+     * @param object $quote
+     * @return float|int
+     */
+    public function getTotalDiscount($quote)
+    {
+        $amount     = 0;
+        $basketData = $this->basketHelper->getBasketSessionValue();
+        if (!empty($basketData)) {
+            $amount = -$basketData->getTotalDiscount();
+        }
+        return $amount;
     }
 }
