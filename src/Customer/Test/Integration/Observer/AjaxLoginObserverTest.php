1c2736e4649c5b8b4162d75baa5f9cc56c0dc023
Umar Yousaf <umaryo@lsretail.com>
2024-07-01 14:20:33 +0800

    [#52991] Added integration tests for customer module

--- src/Customer/Test/Integration/Observer/AjaxLoginObserverTest.php
+++ src/Customer/Test/Integration/Observer/AjaxLoginObserverTest.php
@@ -8,21 +8,21 @@ declare(strict_types=1);
 namespace Ls\Customer\Test\Integration\Observer;
 
 use Laminas\Http\Headers;
-use Ls\Core\Model\LSR;
-use Ls\Customer\Observer\AjaxLoginObserver;
-use Ls\Customer\Test\Fixture\CustomerFixture;
-use Ls\Customer\Test\Integration\AbstractIntegrationTest;
+use \Ls\Core\Model\LSR;
+use \Ls\Customer\Observer\AjaxLoginObserver;
+use \Ls\Customer\Test\Fixture\CustomerFixture;
+use \Ls\Customer\Test\Integration\AbstractIntegrationTest;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Request\Http;
 use Magento\Framework\App\Request\Http as HttpRequest;
-use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Event;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Registry;
-use Magento\TestFramework\Fixture\Config;
 use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\Fixture\Config;
 
 class AjaxLoginObserverTest extends AbstractIntegrationTest
 {
@@ -33,7 +33,7 @@ class AjaxLoginObserverTest extends AbstractIntegrationTest
     public $registry;
     public $controllerAction;
     public $event;
-    public $response;
+    public $fixtures;
 
     protected function setUp(): void
     {
@@ -44,7 +44,7 @@ class AjaxLoginObserverTest extends AbstractIntegrationTest
         $this->registry          = $this->objectManager->get(Registry::class);
         $this->controllerAction  = $this->objectManager->get(Action::class);
         $this->event             = $this->objectManager->get(Event::class);
-        $this->response = $this->objectManager->get(ResponseInterface::class);
+        $this->fixtures          = $this->objectManager->get(DataFixtureStorageManager::class)->getStorage();
     }
 
     /**
@@ -100,7 +100,53 @@ class AjaxLoginObserverTest extends AbstractIntegrationTest
     public function testExecuteWithInValidEmailAndPassword()
     {
         $data = [
-            'username'        => self::USERNAME,
+            'username'        => self::EMAIL,
+            'password'        => self::PASSWORD. '123',
+            'captcha_form_id' => 'user_login',
+            'context'         => 'checkout'
+        ];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setHeaders(Headers::fromString('X_REQUESTED_WITH: XMLHttpRequest'))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+        $content = $this->controllerAction->getResponse()->getContent();
+        $expected = json_encode(['errors' => true, 'message' => 'Invalid Omni login or Omni password']);
+
+        $this->assertEquals(
+            $expected,
+            $content
+        );
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, self::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default')
+    ]
+    public function testExecuteWithInValidUsernameAndPassword()
+    {
+        $data = [
+            'username'        => self::USERNAME. '123',
             'password'        => self::PASSWORD. '123',
             'captcha_form_id' => 'user_login',
             'context'         => 'checkout'
@@ -121,6 +167,310 @@ class AjaxLoginObserverTest extends AbstractIntegrationTest
             ]
         ));
 
+        $content = $this->controllerAction->getResponse()->getContent();
+        $expected = json_encode(['errors' => true, 'message' => 'Invalid Omni login or Omni password']);
+
+        $this->assertEquals(
+            $expected,
+            $content
+        );
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, self::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default')
+    ]
+    public function testExecuteWithNonExistentEmailAndPasswordInBothCentralAndMagento()
+    {
+        $append      = 'test' . substr(sha1((uniqid((string)rand(), true))), 0, 40);
+
+        $data = [
+            'username'        => $append. $append. self::EMAIL,
+            'password'        => self::PASSWORD. '123',
+            'captcha_form_id' => 'user_login',
+            'context'         => 'checkout'
+        ];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setHeaders(Headers::fromString('X_REQUESTED_WITH: XMLHttpRequest'))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+
+        $content = $this->controllerAction->getResponse()->getContent();
+        $expected = json_encode(['errors' => true, 'message' => 'Invalid Omni login or Omni password']);
+
+        $this->assertEquals(
+            $expected,
+            $content
+        );
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, self::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default'),
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username'   => AbstractIntegrationTest::USERNAME,
+                'lsr_id'     => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid' => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token' => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+    ]
+    public function testExecuteWithExistentEmailAndPasswordInBothCentralAndMagento()
+    {
+        $customer = $this->fixtures->get('customer');
+        $data = [
+            'username'        => self::EMAIL,
+            'password'        => self::PASSWORD,
+            'captcha_form_id' => 'user_login',
+            'context'         => 'checkout'
+        ];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setHeaders(Headers::fromString('X_REQUESTED_WITH: XMLHttpRequest'))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+
+        $this->assertEquals($customer->getId(), $this->customerSession->getCustomerId());
+        $this->assertNotNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNotNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNotNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNotNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, self::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_ENABLE, self::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default'),
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username'   => AbstractIntegrationTest::USERNAME,
+                'lsr_id'     => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid' => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token' => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+    ]
+    public function testExecuteWithExistentUsernameAndPasswordInBothCentralAndMagento()
+    {
+        $customer = $this->fixtures->get('customer');
+        $data = [
+            'username'        => self::USERNAME,
+            'password'        => self::PASSWORD,
+            'captcha_form_id' => 'user_login',
+            'context'         => 'checkout'
+        ];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setHeaders(Headers::fromString('X_REQUESTED_WITH: XMLHttpRequest'))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+        $this->assertEquals($customer->getId(), $this->customerSession->getCustomerId());
+        $this->assertNotNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNotNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNotNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNotNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username'   => AbstractIntegrationTest::USERNAME,
+                'lsr_id'     => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid' => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token' => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+    ]
+    public function testExecuteWithUsernameAndLsrDown()
+    {
+        $data = [
+            'username'        => self::USERNAME,
+            'password'        => self::PASSWORD,
+            'captcha_form_id' => 'user_login',
+            'context'         => 'checkout'
+        ];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setHeaders(Headers::fromString('X_REQUESTED_WITH: XMLHttpRequest'))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username'   => AbstractIntegrationTest::USERNAME,
+                'lsr_id'     => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid' => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token' => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+    ]
+    public function testExecuteWithEmailAndLsrDown()
+    {
+        $data = [
+            'username'        => self::EMAIL,
+            'password'        => self::PASSWORD,
+            'captcha_form_id' => 'user_login',
+            'context'         => 'checkout'
+        ];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setHeaders(Headers::fromString('X_REQUESTED_WITH: XMLHttpRequest'))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    public function testExecuteWithoutLoginPayload()
+    {
+        $data = [];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setHeaders(Headers::fromString('X_REQUESTED_WITH: XMLHttpRequest'))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
+        $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
+        $this->assertNull($this->registry->registry(LSR::REGISTRY_LOYALTY_LOGINRESULT));
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    public function testExecuteWithoutAjaxHeader()
+    {
+        $data = [];
+
+        $this->request
+            ->setContent(json_encode($data))
+            ->setMethod(Http::METHOD_POST);
+        $this->event->setData([
+            'request' => $this->request
+        ]);
+
+        $this->ajaxLoginObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
         $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_LSRID));
         $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_CARDID));
         $this->assertNull($this->customerSession->getData(LSR::SESSION_CUSTOMER_SECURITYTOKEN));
---
+++ src/Customer/Test/Integration/Observer/LogoutObserverTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Ls\Customer\Test\Integration\Observer;
+
+use \Ls\Customer\Observer\LogoutObserver;
+use \Ls\Customer\Test\Fixture\CustomerFixture;
+use \Ls\Customer\Test\Integration\AbstractIntegrationTest;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Request\Http as HttpRequest;
+use Magento\Framework\Event;
+use Magento\Framework\Event\Observer;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Helper\Bootstrap;
+
+class LogoutObserverTest extends AbstractIntegrationTest
+{
+    public $objectManager;
+    public $request;
+    public $logoutObserver;
+    public $customerSession;
+    public $controllerAction;
+    public $event;
+    public $fixtures;
+
+    protected function setUp(): void
+    {
+        $this->objectManager    = Bootstrap::getObjectManager();
+        $this->request          = $this->objectManager->get(HttpRequest::class);
+        $this->logoutObserver   = $this->objectManager->get(LogoutObserver::class);
+        $this->customerSession  = $this->objectManager->get(CustomerSession::class);
+        $this->controllerAction = $this->objectManager->get(Action::class);
+        $this->event            = $this->objectManager->get(Event::class);
+        $this->fixtures         = $this->objectManager->get(DataFixtureStorageManager::class)->getStorage();
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username' => AbstractIntegrationTest::USERNAME,
+                'lsr_id'       => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid'   => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token'    => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+    ]
+    public function testExecuteToCheckEmptySession()
+    {
+        $customer = $this->fixtures->get('customer');
+        $this->customerSession->setData('customer_id', $customer->getId());
+        $this->logoutObserver->execute(new Observer(
+            [
+                'event' => $this->event,
+                'controller_action' => $this->controllerAction,
+                'request' => $this->request
+            ]
+        ));
+
+        $this->assertTrue(count($this->customerSession->getData()) === 0);
+    }
+}
