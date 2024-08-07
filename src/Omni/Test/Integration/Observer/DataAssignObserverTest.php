10cdc334c64e7d9cf9c1e983f1ed2b2f0ef211f3
Deepak K <deepakkna@lsretail.com>
2024-08-07 17:37:19 +0800

    52992 DataAssignObserver Integration test

--- src/Omni/Test/Integration/Observer/DataAssignObserverTest.php
+++ src/Omni/Test/Integration/Observer/DataAssignObserverTest.php
@@ -8,39 +8,23 @@ declare(strict_types=1);
 namespace Ls\Omni\Test\Integration\Observer;
 
 use \Ls\Core\Model\LSR;
-use Ls\Omni\Test\Fixture\ApplyLoyaltyPointsInCartFixture;
+use \Ls\Omni\Test\Fixture\ApplyLoyaltyPointsInCartFixture;
 use \Ls\Omni\Test\Fixture\CustomerAddressFixture;
 use \Ls\Omni\Test\Fixture\CustomerOrder;
-use \Ls\Omni\Test\Fixture\OrderCreateFixture;
 use \Ls\Omni\Helper\BasketHelper;
-use \Ls\Omni\Helper\ContactHelper;
 use \Ls\Omni\Helper\LoyaltyHelper;
-use \Ls\Omni\Model\Payment\PayStore;
 use \Ls\Omni\Observer\DataAssignObserver;
 use \Ls\Omni\Test\Fixture\CreateSimpleProductFixture;
 use \Ls\Customer\Test\Fixture\CustomerFixture;
 use \Ls\Omni\Test\Integration\AbstractIntegrationTest;
-use Magento\Checkout\Test\Fixture\PlaceOrder as PlaceOrderFixture;
-use Magento\Checkout\Test\Fixture\SetBillingAddress as SetBillingAddress;
-use Magento\Checkout\Test\Fixture\SetDeliveryMethod;
-use Magento\Checkout\Test\Fixture\SetDeliveryMethod as SetDeliveryMethodFixture;
-use Magento\Checkout\Test\Fixture\SetPaymentMethod as SetPaymentMethod;
-use Magento\Checkout\Test\Fixture\SetPaymentMethod as SetPaymentMethodFixture;
 use Magento\Framework\Exception\ValidatorException;
-use Magento\Quote\Model\Quote\Address\Total\CollectorInterface;
-use Magento\Quote\Model\Quote\Address\TotalFactory;
-use Magento\Checkout\Test\Fixture\SetShippingAddress as SetShippingAddress;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Request\Http as HttpRequest;
-use Magento\Framework\DataObject;
-use Magento\Framework\Event;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Registry;
-use Magento\OfflinePayments\Model\Checkmo;
-use Magento\Payment\Model\Method\Free;
 use Magento\Quote\Test\Fixture\AddProductToCart;
 use Magento\Quote\Test\Fixture\CustomerCart;
 use Magento\TestFramework\Fixture\Config;
@@ -49,8 +33,6 @@ use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\Quote\Model\QuoteFactory;
 use Magento\TestFramework\Fixture\AppArea;
-use Magento\Quote\Model\QuoteIdMaskFactory;
-use Magento\Quote\Model\Quote\TotalsCollectorList;
 
 class DataAssignObserverTest extends AbstractIntegrationTest
 {
@@ -92,32 +74,7 @@ class DataAssignObserverTest extends AbstractIntegrationTest
     /**
      * @var mixed
      */
-    public $contactHelper;
-
-    /**
-     * @var mixed
-     */
     public $basketHelper;
-
-    /**
-     * @var mixed
-     */
-    public $checkmo;
-
-    /**
-     * @var mixed
-     */
-    public $payAtStore;
-
-    /**
-     * @var Free
-     */
-    public $free;
-
-    /**
-     * @var mixed
-     */
-    public $event;
 
     /**
      * @var mixed
@@ -128,21 +85,6 @@ class DataAssignObserverTest extends AbstractIntegrationTest
      * @var DataAssignObserver
      */
     public $dataAssignObserver;
-
-    /**
-     * @var QuoteIdMaskFactory
-     */
-    public $quoteIdMaskFactory;
-
-    /**
-     * @var mixed
-     */
-    public $collectorList;
-
-    /**
-     * @var TotalFactory
-     */
-    public $totalFactory;
 
     /**
      * @var LoyaltyHelper
@@ -179,18 +121,9 @@ class DataAssignObserverTest extends AbstractIntegrationTest
         $this->customerSession    = $this->objectManager->get(CustomerSession::class);
         $this->checkoutSession    = $this->objectManager->get(CheckoutSession::class);
         $this->controllerAction   = $this->objectManager->get(Action::class);
-        $this->contactHelper      = $this->objectManager->get(ContactHelper::class);
         $this->basketHelper       = $this->objectManager->get(BasketHelper::class);
         $this->eventManager       = $this->objectManager->create(ManagerInterface::class);
-        $this->event              = $this->objectManager->get(Event::class);
         $this->dataAssignObserver = $this->objectManager->get(DataAssignObserver::class);
-        $this->checkmo            = $this->objectManager->get(Checkmo::class);
-        $this->payAtStore         = $this->objectManager->get(PayStore::class);
-        $this->free               = $this->objectManager->get(Free::class);
-        $this->quoteIdMaskFactory = $this->objectManager->get(QuoteIdMaskFactory::class);
-        $this->totalFactory       = $this->objectManager->get(TotalFactory::class);
-        $this->collectorList      = $this->objectManager->get(TotalsCollectorList::class);
-        $this->loyaltyHelper      = $this->objectManager->get(LoyaltyHelper::class);
     }
 
     /**
