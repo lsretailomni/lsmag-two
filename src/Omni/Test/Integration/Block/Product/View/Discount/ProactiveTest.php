3b10cc89f99ccebaa12ac4950a653ff77755f718
Deepak K <deepakkna@lsretail.com>
2024-09-18 10:51:32 +0800

    52992 Proactive Block integration test

---
+++ src/Omni/Test/Integration/Block/Product/View/Discount/ProactiveTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Ls\Omni\Test\Integration\Block\Product\View\Discount;
+
+use Ls\Core\Model\LSR;
+use \Ls\Omni\Block\Product\View\Discount\Proactive;
+use \Ls\Omni\Helper\ContactHelper;
+use \Ls\Omni\Test\Integration\AbstractIntegrationTest;
+use Magento\Framework\DataObject;
+use Magento\TestFramework\Fixture\AppArea;
+use Magento\TestFramework\Fixture\Config;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * @magentoAppArea adminhtml
+ */
+class ProactiveTest extends AbstractIntegrationTest
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    public $objectManager;
+
+    /**
+     * @var mixed
+     */
+    public $defaultTotal;
+
+    /**
+     * @var Proactive
+     */
+    public $proactive;
+
+    /**
+     * @var ContactHelper
+     */
+    public $contactHelper;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->proactive     = $this->objectManager->get(Proactive::class);
+        $this->contactHelper = $this->objectManager->get(ContactHelper::class);
+    }
+
+//    /**
+//     * @magentoAppIsolation enabled
+//     */
+//    #[
+//        AppArea('frontend'),
+//        Config(LSR::SC_SERVICE_ENABLE, self::LS_MAG_ENABLE, 'store', 'default'),
+//        Config(LSR::SC_SERVICE_BASE_URL, self::CS_URL, 'store', 'default'),
+//        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+//        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default'),
+//        Config(LSR::LS_INDUSTRY_VALUE, self::RETAIL_INDUSTRY, 'store', 'default'),
+//        Config(LSR::SC_SERVICE_LS_CENTRAL_VERSION, AbstractIntegrationTest::LICENSE, 'website')
+//    ]
+//    public function testGetProactiveDiscounts()
+//    {
+//        $result = $this->proactive->getProactiveDiscounts(40100);
+//
+//        $this->assertNotEquals([], $result);
+//    }
+//
+//    /**
+//     * @magentoAppIsolation enabled
+//     */
+//    #[
+//        AppArea('frontend'),
+//        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+//        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default'),
+//        Config(LSR::LS_INDUSTRY_VALUE, self::RETAIL_INDUSTRY, 'store', 'default'),
+//        Config(LSR::SC_SERVICE_LS_CENTRAL_VERSION, AbstractIntegrationTest::LICENSE, 'website')
+//    ]
+//    public function testGetProactiveDiscountsWithLSRDisabled()
+//    {
+//        $result = $this->proactive->getProactiveDiscounts(40100);
+//
+//        $this->assertEquals([], $result);
+//    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        AppArea('frontend'),
+        Config(LSR::SC_SERVICE_ENABLE, self::LS_MAG_ENABLE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, self::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default'),
+        Config(LSR::LS_INDUSTRY_VALUE, self::RETAIL_INDUSTRY, 'store', 'default'),
+        Config(LSR::SC_SERVICE_LS_CENTRAL_VERSION, AbstractIntegrationTest::LICENSE, 'website')
+    ]
+    public function testGetCoupons()
+    {
+        $this->contactHelper->setCardIdInCustomerSession(self::LSR_CARD_ID);
+        $result = $this->proactive->getCoupons([40180]);
+
+        $this->assertNotEquals([], $result);
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        AppArea('frontend'),
+        Config(LSR::SC_SERVICE_ENABLE, self::LS_MAG_ENABLE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, self::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, self::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, self::CS_VERSION, 'store', 'default'),
+        Config(LSR::LS_INDUSTRY_VALUE, self::RETAIL_INDUSTRY, 'store', 'default'),
+        Config(LSR::SC_SERVICE_LS_CENTRAL_VERSION, AbstractIntegrationTest::LICENSE, 'website')
+    ]
+    public function testGetCouponsWithoutCardId()
+    {
+        $this->contactHelper->setCardIdInCustomerSession('');
+        $result = $this->proactive->getCoupons([40180]);
+
+        $this->assertEquals([], $result);
+    }
+}
