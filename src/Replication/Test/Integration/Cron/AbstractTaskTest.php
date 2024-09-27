5931538e27161a6b0ddd832871affb48ebced14e
Umar yousaf <umaryo@lsretail.com>
2024-09-27 12:31:07 +0800

    [#52990] Added integration tests for replication module

--- src/Replication/Test/Integration/Cron/AbstractTaskTest.php
+++ src/Replication/Test/Integration/Cron/AbstractTaskTest.php
@@ -17,10 +17,32 @@ use \Ls\Replication\Api\ReplItemRepositoryInterface;
 use \Ls\Replication\Api\ReplItemUnitOfMeasureRepositoryInterface;
 use \Ls\Replication\Api\ReplItemVariantRegistrationRepositoryInterface;
 use \Ls\Replication\Api\ReplItemVariantRepositoryInterface;
+use \Ls\Replication\Api\ReplLoyVendorItemMappingRepositoryInterface;
+use \Ls\Replication\Api\ReplVendorRepositoryInterface;
+use \Ls\Replication\Cron\AttributesCreateTask;
+use \Ls\Replication\Cron\CategoryCreateTask;
 use \Ls\Replication\Cron\ProductCreateTask;
+use \Ls\Replication\Cron\ReplEcommAttributeOptionValueTask;
+use \Ls\Replication\Cron\ReplEcommAttributeTask;
+use \Ls\Replication\Cron\ReplEcommAttributeValueTask;
+use \Ls\Replication\Cron\ReplEcommBarcodesTask;
+use \Ls\Replication\Cron\ReplEcommExtendedVariantsTask;
+use \Ls\Replication\Cron\ReplEcommHierarchyLeafTask;
+use \Ls\Replication\Cron\ReplEcommHierarchyNodeTask;
+use \Ls\Replication\Cron\ReplEcommImageLinksTask;
+use \Ls\Replication\Cron\ReplEcommInventoryStatusTask;
+use \Ls\Replication\Cron\ReplEcommItemsTask;
+use \Ls\Replication\Cron\ReplEcommItemUnitOfMeasuresTask;
+use \Ls\Replication\Cron\ReplEcommItemVariantRegistrationsTask;
+use \Ls\Replication\Cron\ReplEcommItemVariantsTask;
+use \Ls\Replication\Cron\ReplEcommPricesTask;
+use \Ls\Replication\Cron\ReplEcommUnitOfMeasuresTask;
+use \Ls\Replication\Cron\ReplEcommVendorItemMappingTask;
+use \Ls\Replication\Cron\ReplEcommVendorTask;
 use \Ls\Replication\Helper\ReplicationHelper;
 use \Ls\Replication\Api\ReplPriceRepositoryInterface as ReplPriceRepository;
 use \Ls\Replication\Model\ReplItem;
+use \Ls\Replication\Test\Fixture\FlatDataReplication;
 use \Ls\Replication\Test\Integration\AbstractIntegrationTest;
 use Magento\Catalog\Api\CategoryRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
@@ -31,10 +53,145 @@ use Magento\Framework\App\ResourceConnection;
 use Magento\InventoryCatalogAdminUi\Model\GetSourceItemsDataBySku;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\TestFramework\Fixture\Config;
+use Magento\TestFramework\Fixture\DataFixture;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
 
-class AbstractTask extends TestCase
+/**
+ * @magentoAppArea crontab
+ * @magentoDbIsolation disabled
+ * @magentoAppIsolation enabled
+ */
+#[
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommAttributeTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommAttributeOptionValueTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommAttributeValueTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommExtendedVariantsTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommItemVariantsTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommUnitOfMeasuresTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommItemUnitOfMeasuresTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommVendorTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommHierarchyNodeTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommHierarchyLeafTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommItemsTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommBarcodesTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommItemVariantRegistrationsTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommPricesTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommImageLinksTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommInventoryStatusTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommVendorTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+    DataFixture(
+        FlatDataReplication::class,
+        [
+            'job_url' => ReplEcommVendorItemMappingTask::class,
+            'scope' => ScopeInterface::SCOPE_WEBSITE
+        ]
+    ),
+]
+abstract class AbstractTaskTest extends TestCase
 {
     public $lsTables = [
         ['table' => 'ls_replication_repl_item', 'id' => 'nav_id'],
@@ -89,6 +246,8 @@ class AbstractTask extends TestCase
     public $stockRegistry;
     public $replAttributeValueRepository;
     public $replAttributeValueInterfaceFactory;
+    public $replVendorItemRepository;
+    public $replVendorRepository;
 
     /**
      * @inheritdoc
@@ -121,6 +280,56 @@ class AbstractTask extends TestCase
         $this->sourceItems                           = $this->objectManager->get(GetSourceItemsDataBySku::class);
         $this->stockRegistry                         = $this->objectManager->get(StockRegistryStorage::class);
         $this->replAttributeValueRepository          = $this->objectManager->get(ReplAttributeValueRepositoryInterface::class);
+        $this->replVendorItemRepository              = $this->objectManager->get(ReplLoyVendorItemMappingRepositoryInterface::class);
+        $this->replVendorRepository                  = $this->objectManager->get(ReplVendorRepositoryInterface::class);
+    }
+
+    /**
+     * @magentoDbIsolation disabled
+     */
+    #[
+        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::ENABLED, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'website'),
+        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::ENABLED, 'website'),
+        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'website'),
+        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'website'),
+        Config(LSR::LS_INDUSTRY_VALUE, LSR::LS_INDUSTRY_VALUE_RETAIL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_LS_CENTRAL_VERSION, AbstractIntegrationTest::LS_VERSION, 'website'),
+        Config(LSR::SC_REPLICATION_DEFAULT_BATCHSIZE, AbstractIntegrationTest::DEFAULT_BATCH_SIZE),
+        Config(
+            LSR::SC_REPLICATION_HIERARCHY_CODE,
+            AbstractIntegrationTest::SAMPLE_HIERARCHY_NAV_ID,
+            'store',
+            'default'
+        ),
+        Config(LSR::SC_REPLICATION_PRODUCT_BATCHSIZE, 5, 'store', 'default')
+    ]
+    public function testExecute()
+    {
+        $this->cron->store = $this->storeManager->getStore();
+        $this->cron->webStoreId = AbstractIntegrationTest::CS_STORE;
+        $this->addDummyDataStandardVariant();
+        $this->executePreReqCrons();
+        $this->actualExecute();
+    }
+
+    public abstract function actualExecute();
+
+    public function executePreReqCrons()
+    {
+        $this->executeUntilReady(AttributesCreateTask::class, [
+            LSR::SC_SUCCESS_CRON_ATTRIBUTE,
+            LSR::SC_SUCCESS_CRON_ATTRIBUTE_VARIANT,
+            LSR::SC_SUCCESS_CRON_ATTRIBUTE_STANDARD_VARIANT
+        ]);
+
+        $this->executeUntilReady(CategoryCreateTask::class, [
+            LSR::SC_SUCCESS_CRON_CATEGORY
+        ]);
+        $this->updateAllRelevantItemRecords();
     }
 
     public function executeUntilReady($cronClass, $successStatus)
@@ -131,6 +340,25 @@ class AbstractTask extends TestCase
 
             if ($this->isReady($successStatus, $this->storeManager->getStore()->getId())) {
                 break;
+            }
+        }
+    }
+
+    public function assertCronSuccess($cronConfigs, $storeId, $status = true)
+    {
+        foreach ($cronConfigs as $config) {
+            if (!$status) {
+                $this->assertFalse((bool)$this->lsr->getConfigValueFromDb(
+                    $config,
+                    ScopeInterface::SCOPE_STORES,
+                    $storeId
+                ));
+            } else {
+                $this->assertTrue((bool)$this->lsr->getConfigValueFromDb(
+                    $config,
+                    ScopeInterface::SCOPE_STORES,
+                    $storeId
+                ));
             }
         }
     }
--- src/Replication/Test/Integration/Cron/SyncAttributesValueTest.php
+++ src/Replication/Test/Integration/Cron/SyncAttributesValueTest.php
@@ -4,153 +4,13 @@ declare(strict_types=1);
 namespace Ls\Replication\Test\Integration\Cron;
 
 use \Ls\Core\Model\LSR;
-use \Ls\Replication\Cron\AttributesCreateTask;
-use \Ls\Replication\Cron\CategoryCreateTask;
 use \Ls\Replication\Cron\ProductCreateTask;
-use \Ls\Replication\Cron\ReplEcommAttributeOptionValueTask;
-use \Ls\Replication\Cron\ReplEcommAttributeTask;
-use \Ls\Replication\Cron\ReplEcommAttributeValueTask;
-use \Ls\Replication\Cron\ReplEcommBarcodesTask;
-use \Ls\Replication\Cron\ReplEcommExtendedVariantsTask;
-use \Ls\Replication\Cron\ReplEcommHierarchyLeafTask;
-use \Ls\Replication\Cron\ReplEcommHierarchyNodeTask;
-use \Ls\Replication\Cron\ReplEcommImageLinksTask;
-use \Ls\Replication\Cron\ReplEcommInventoryStatusTask;
-use \Ls\Replication\Cron\ReplEcommItemsTask;
-use \Ls\Replication\Cron\ReplEcommItemUnitOfMeasuresTask;
-use \Ls\Replication\Cron\ReplEcommItemVariantRegistrationsTask;
-use \Ls\Replication\Cron\ReplEcommItemVariantsTask;
-use \Ls\Replication\Cron\ReplEcommPricesTask;
-use \Ls\Replication\Cron\ReplEcommUnitOfMeasuresTask;
-use \Ls\Replication\Cron\ReplEcommVendorTask;
 use \Ls\Replication\Cron\SyncAttributesValue;
-use \Ls\Replication\Test\Fixture\FlatDataReplication;
 use \Ls\Replication\Test\Integration\AbstractIntegrationTest;
 use Magento\Catalog\Model\Product\Type;
 use Magento\Store\Model\ScopeInterface;
-use Magento\TestFramework\Fixture\Config;
-use Magento\TestFramework\Fixture\DataFixture;
 
-/**
- * @magentoAppArea crontab
- * @magentoDbIsolation disabled
- * @magentoAppIsolation enabled
- */
-#[
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommAttributeTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommAttributeOptionValueTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommAttributeValueTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommExtendedVariantsTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommItemVariantsTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommUnitOfMeasuresTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommItemUnitOfMeasuresTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommVendorTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommHierarchyNodeTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommHierarchyLeafTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommItemsTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommBarcodesTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommItemVariantRegistrationsTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommPricesTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommImageLinksTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    ),
-    DataFixture(
-        FlatDataReplication::class,
-        [
-            'job_url' => ReplEcommInventoryStatusTask::class,
-            'scope' => ScopeInterface::SCOPE_WEBSITE
-        ]
-    )
-]
-class SyncAttributesValueTest extends AbstractTask
+class SyncAttributesValueTest extends AbstractTaskTest
 {
     /**
      * @inheritdoc
@@ -168,30 +28,7 @@ class SyncAttributesValueTest extends AbstractTask
         parent::tearDown();
     }
 
-    /**
-     * @magentoDbIsolation disabled
-     */
-    #[
-        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::ENABLED, 'store', 'default'),
-        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'store', 'default'),
-        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'store', 'default'),
-        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'store', 'default'),
-        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'website'),
-        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::ENABLED, 'website'),
-        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'website'),
-        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'website'),
-        Config(LSR::LS_INDUSTRY_VALUE, LSR::LS_INDUSTRY_VALUE_RETAIL, 'store', 'default'),
-        Config(LSR::SC_SERVICE_LS_CENTRAL_VERSION, AbstractIntegrationTest::LS_VERSION, 'website'),
-        Config(LSR::SC_REPLICATION_DEFAULT_BATCHSIZE, AbstractIntegrationTest::DEFAULT_BATCH_SIZE),
-        Config(
-            LSR::SC_REPLICATION_HIERARCHY_CODE,
-            AbstractIntegrationTest::SAMPLE_HIERARCHY_NAV_ID,
-            'store',
-            'default'
-        ),
-        Config(LSR::SC_REPLICATION_PRODUCT_BATCHSIZE, 5, 'store', 'default')
-    ]
-    public function testExecute()
+    public function actualExecute()
     {
         $storeId           = $this->storeManager->getStore()->getId();
         $this->cron->store = $this->storeManager->getStore();
@@ -325,38 +162,5 @@ class SyncAttributesValueTest extends AbstractTask
         }
 
         $this->replAttributeValueRepository->save($softAttribute);
-    }
-
-    public function executePreReqCrons()
-    {
-        $this->executeUntilReady(AttributesCreateTask::class, [
-            LSR::SC_SUCCESS_CRON_ATTRIBUTE,
-            LSR::SC_SUCCESS_CRON_ATTRIBUTE_VARIANT,
-            LSR::SC_SUCCESS_CRON_ATTRIBUTE_STANDARD_VARIANT
-        ]);
-
-        $this->executeUntilReady(CategoryCreateTask::class, [
-            LSR::SC_SUCCESS_CRON_CATEGORY
-        ]);
-        $this->updateAllRelevantItemRecords();
-    }
-
-    public function assertCronSuccess($cronConfigs, $storeId, $status = true)
-    {
-        foreach ($cronConfigs as $config) {
-            if (!$status) {
-                $this->assertFalse((bool)$this->lsr->getConfigValueFromDb(
-                    $config,
-                    ScopeInterface::SCOPE_STORES,
-                    $storeId
-                ));
-            } else {
-                $this->assertTrue((bool)$this->lsr->getConfigValueFromDb(
-                    $config,
-                    ScopeInterface::SCOPE_STORES,
-                    $storeId
-                ));
-            }
-        }
     }
 }
