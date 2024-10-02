5904fd797a120b800a01c27946d69747223deb45
Deepak K <deepakkna@lsretail.com>
2024-10-02 12:02:31 +0800

    52992 Product controller integration test code fix

--- src/Omni/Test/Integration/Controller/Stock/ProductTest.php
+++ src/Omni/Test/Integration/Controller/Stock/ProductTest.php
@@ -3,49 +3,76 @@
 namespace Ls\Omni\Test\Integration\Controller\Stock;
 
 use \Ls\Core\Model\LSR;
-use \Ls\Customer\Test\Fixture\CustomerFixture;
+use \Ls\Omni\Test\Fixture\FlatDataReplication;
 use \Ls\Omni\Test\Fixture\CreateSimpleProductFixture;
 use \Ls\Omni\Test\Integration\AbstractIntegrationTest;
+use \Ls\Replication\Cron\ReplEcommStoresTask;
 use Magento\Framework\App\Request\Http as HttpRequest;
-use Magento\TestFramework\Fixture\AppArea;
 use Magento\TestFramework\Fixture\Config;
 use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\TestCase\AbstractController;
+use Magento\Store\Model\ScopeInterface;
 
 class ProductTest extends AbstractController
 {
     /**
+     * @var DataFixtureStorageManager
+     */
+    public $fixtures;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    public $objectManager;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->fixtures      = $this->objectManager->get(DataFixtureStorageManager::class)->getStorage();
+    }
+
+    /**
      * @magentoAppIsolation enabled
      */
     #[
-        AppArea('frontend'),
         Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::LS_MAG_ENABLE, 'store', 'default'),
         Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'store', 'default'),
         Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'store', 'default'),
         Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'store', 'default'),
         Config(LSR::LS_INDUSTRY_VALUE, AbstractIntegrationTest::RETAIL_INDUSTRY, 'store', 'default'),
-        DataFixture(
-            CustomerFixture::class,
-            [
-                'lsr_username' => AbstractIntegrationTest::USERNAME,
-                'lsr_id'       => AbstractIntegrationTest::LSR_ID,
-                'lsr_cardid'   => AbstractIntegrationTest::LSR_CARD_ID,
-                'lsr_token'    => AbstractIntegrationTest::CUSTOMER_ID
-            ],
-            as: 'customer'
-        ),
+
+        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::LS_MAG_ENABLE, 'website'),
+        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'website'),
+        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'website'),
+        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'website'),
         DataFixture(
             CreateSimpleProductFixture::class,
             [
                 LSR::LS_ITEM_ID_ATTRIBUTE_CODE => '40180',
             ],
             as: 'p1'
+        ),
+        DataFixture(
+            FlatDataReplication::class,
+            [
+                'job_url' => ReplEcommStoresTask::class,
+                'scope'   => ScopeInterface::SCOPE_WEBSITE
+            ],
+            as: 'stores'
         )
     ]
     public function testExecute()
     {
+        $prod   = $this->fixtures->get('p1');
+        $stores = $this->fixtures->get('stores');
         $params = [
-            'sku' => '40180',
+            'sku' => $prod->getSku(),
             'id'  => ''
         ];
         $this->getRequest()->setParams($params);
@@ -57,5 +84,6 @@ class ProductTest extends AbstractController
         $content = json_decode($this->getResponse()->getBody());
         $this->assertNotNull($content->content);
         $this->assertNotNull($content->stocks);
+        $this->assertStringContainsString("totalRecords", $content->stocks);
     }
 }
