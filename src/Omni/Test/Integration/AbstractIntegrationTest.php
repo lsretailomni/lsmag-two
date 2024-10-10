c4f23be27f2dbdce9eb237ae90e07c5b4c0b3c71
Deepak K <deepakkna@lsretail.com>
2024-10-10 10:54:21 +0800

    52992 DiscountManagement Integration test update

--- src/Omni/Test/Integration/AbstractIntegrationTest.php
+++ src/Omni/Test/Integration/AbstractIntegrationTest.php
@@ -30,7 +30,9 @@ class AbstractIntegrationTest extends TestCase
     public const VALID_COUPON_CODE = 'COUP0119';
     public const INVALID_COUPON_CODE = 'COUPON_CODE';
     public const GIFTCARD = '10000011';
+    public const GIFTCARD_EXPIRED = '10000002';
     public const GIFTCARD_PIN = '8118';
+    public const GIFTCARD_EXPIRED_PIN = '2979';
     public const GIFTCARD_AMOUNT = '1';
     public const LOY_POINTS = '2';
     public const LICENSE = '25.0.0.0 (25.0.0.0 [18056] CL:True EL:True)';
