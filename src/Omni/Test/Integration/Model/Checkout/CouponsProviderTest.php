1e9e87b72382abeda3969563eacece9091807d8a
Deepak K <deepakkna@lsretail.com>
2024-10-11 11:43:34 +0800

    52992 src/Omni/Test/Integration/Model/Checkout/CouponsProviderTest.php

---
+++ src/Omni/Test/Integration/Model/Checkout/CouponsProviderTest.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Ls\Omni\Test\Integration\Model\Checkout;
+
+use \Ls\Core\Model\LSR;
+use \Ls\Customer\Test\Fixture\CustomerFixture;
+use \Ls\Omni\Helper\ContactHelper;
+use \Ls\Omni\Test\Fixture\CreateSimpleProductFixture;
+use \Ls\Omni\Test\Integration\AbstractIntegrationTest;
+use \Ls\Omni\Model\Checkout\CouponsProvider;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Registry;
+use Magento\Quote\Test\Fixture\AddProductToCart;
+use Magento\Quote\Test\Fixture\CustomerCart;
+use Magento\TestFramework\Fixture\Config;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\Fixture\AppArea;
+use PHPUnit\Framework\TestCase;
+
+class CouponsProviderTest extends TestCase
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    public $objectManager;
+
+    /**
+     * @var mixed
+     */
+    public $request;
+
+    /**
+     * @var DataFixtureStorageManager
+     */
+    public $fixtures;
+
+    public $registry;
+    public $customerSession;
+    public $checkoutSession;
+    public $contactHelper;
+    public $couponsProvider;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->objectManager   = Bootstrap::getObjectManager();
+        $this->fixtures        = $this->objectManager->get(DataFixtureStorageManager::class)->getStorage();
+        $this->couponsProvider = $this->objectManager->get(CouponsProvider::class);
+        $this->registry        = $this->objectManager->get(Registry::class);
+        $this->customerSession = $this->objectManager->get(CustomerSession::class);
+        $this->contactHelper   = $this->objectManager->get(ContactHelper::class);
+        $this->checkoutSession = $this->objectManager->get(CheckoutSession::class);
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        AppArea('frontend'),
+        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::LS_MAG_ENABLE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'store', 'default'),
+        Config(LSR::LS_INDUSTRY_VALUE, AbstractIntegrationTest::RETAIL_INDUSTRY, 'store', 'default'),
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username' => AbstractIntegrationTest::USERNAME,
+                'lsr_id'       => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid'   => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token'    => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+        DataFixture(
+            CreateSimpleProductFixture::class,
+            [
+                LSR::LS_ITEM_ID_ATTRIBUTE_CODE => '40180',
+            ],
+            as: 'p1'
+        ),
+        DataFixture(CustomerCart::class, ['customer_id' => '$customer.id$'], 'cart1'),
+        DataFixture(AddProductToCart::class, ['cart_id' => '$cart1.id$', 'product_id' => '$p1.id$', 'qty' => 1])
+    ]
+    public function testGetConfig()
+    {
+        $customer = $this->fixtures->get('customer');
+        $cart     = $this->fixtures->get('cart1');
+        $this->customerSession->setData('customer_id', $customer->getId());
+        $this->customerSession->setData(LSR::SESSION_CUSTOMER_CARDID, $customer->getLsrCardid());
+        $this->checkoutSession->setQuoteId($cart->getId());
+
+        $result = $this->contactHelper->login(AbstractIntegrationTest::USERNAME, AbstractIntegrationTest::PASSWORD);
+        $this->registry->register(LSR::REGISTRY_LOYALTY_LOGINRESULT, $result);
+
+        $result = $this->couponsProvider->getConfig();
+
+        $this->assertArrayHasKey('0', $result['coupons']);
+        $this->assertArrayHasKey('discount_code', $result['coupons']['0']);
+        $this->assertNotNull($result['coupons']['0']['discount_code']);
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        AppArea('frontend'),
+        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::LS_MAG_ENABLE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'store', 'default'),
+        Config(LSR::LS_INDUSTRY_VALUE, AbstractIntegrationTest::RETAIL_INDUSTRY, 'store', 'default'),
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username' => AbstractIntegrationTest::USERNAME,
+                'lsr_id'       => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid'   => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token'    => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+        DataFixture(
+            CreateSimpleProductFixture::class,
+            [
+                LSR::LS_ITEM_ID_ATTRIBUTE_CODE => '40170',
+            ],
+            as: 'p1'
+        ),
+        DataFixture(CustomerCart::class, ['customer_id' => '$customer.id$'], 'cart1'),
+        DataFixture(AddProductToCart::class, ['cart_id' => '$cart1.id$', 'product_id' => '$p1.id$', 'qty' => 1])
+    ]
+    public function testGetConfigNoCoupons()
+    {
+        $customer = $this->fixtures->get('customer');
+        $cart     = $this->fixtures->get('cart1');
+        $this->customerSession->setData('customer_id', $customer->getId());
+        $this->customerSession->setData(LSR::SESSION_CUSTOMER_CARDID, $customer->getLsrCardid());
+        $this->checkoutSession->setQuoteId($cart->getId());
+
+        $result = $this->contactHelper->login(AbstractIntegrationTest::USERNAME, AbstractIntegrationTest::PASSWORD);
+        $this->registry->register(LSR::REGISTRY_LOYALTY_LOGINRESULT, $result);
+
+        $result = $this->couponsProvider->getConfig();
+
+        $this->assertNotNull($result);
+        $this->assertArrayNotHasKey('0', $result['coupons']);
+    }
+}
