6fe0272cdfe8cb6440ab4109a4fea83129bcab80
Deepak K <deepakkna@lsretail.com>
2024-10-15 11:05:31 +0800

    52992 GiftCardManagement Integration Test

---
+++ src/Omni/Test/Integration/Model/GiftCard/GiftCardManagementTest.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Ls\Omni\Test\Integration\Model\GiftCard;
+
+use \Ls\Core\Model\LSR;
+use \Ls\Customer\Test\Fixture\CustomerFixture;
+use \Ls\Omni\Test\Fixture\CreateSimpleProductFixture;
+use \Ls\Omni\Test\Integration\AbstractIntegrationTest;
+use \Ls\Omni\Model\GiftCard\GiftCardManagement;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Registry;
+use Magento\Quote\Test\Fixture\AddProductToCart;
+use Magento\Quote\Test\Fixture\CustomerCart;
+use Magento\TestFramework\Fixture\Config;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\Fixture\AppArea;
+use PHPUnit\Framework\TestCase;
+
+class GiftCardManagementTest extends TestCase
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    public $objectManager;
+
+    /**
+     * @var mixed
+     */
+    public $request;
+
+    /**
+     * @var DataFixtureStorageManager
+     */
+    public $fixtures;
+
+    public $registry;
+    public $customerSession;
+    public $checkoutSession;
+    public $eventManager;
+    public $giftCardManagement;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->objectManager      = Bootstrap::getObjectManager();
+        $this->fixtures           = $this->objectManager->get(DataFixtureStorageManager::class)->getStorage();
+        $this->giftCardManagement = $this->objectManager->get(GiftCardManagement::class);
+        $this->registry           = $this->objectManager->get(Registry::class);
+        $this->customerSession    = $this->objectManager->get(CustomerSession::class);
+        $this->checkoutSession    = $this->objectManager->get(CheckoutSession::class);
+        $this->eventManager       = $this->objectManager->create(ManagerInterface::class);
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        AppArea('frontend'),
+        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::LS_MAG_ENABLE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'store', 'default'),
+        Config(LSR::LS_INDUSTRY_VALUE, AbstractIntegrationTest::RETAIL_INDUSTRY, 'store', 'default'),
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username' => AbstractIntegrationTest::USERNAME,
+                'lsr_id'       => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid'   => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token'    => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+        DataFixture(
+            CreateSimpleProductFixture::class,
+            [
+                LSR::LS_ITEM_ID_ATTRIBUTE_CODE => '40180',
+            ],
+            as: 'p1'
+        ),
+        DataFixture(CustomerCart::class, ['customer_id' => '$customer.id$'], 'cart1'),
+        DataFixture(AddProductToCart::class, ['cart_id' => '$cart1.id$', 'product_id' => '$p1.id$', 'qty' => 1])
+    ]
+    public function testApplyValidGiftCard()
+    {
+        $customer = $this->fixtures->get('customer');
+        $cart     = $this->fixtures->get('cart1');
+        $this->customerSession->setData('customer_id', $customer->getId());
+        $this->customerSession->setData(LSR::SESSION_CUSTOMER_CARDID, $customer->getLsrCardid());
+        $this->checkoutSession->setQuoteId($cart->getId());
+
+        $this->eventManager->dispatch('checkout_cart_save_after', ['items' => $cart->getAllVisibleItems()]);
+
+        $result = $this->giftCardManagement->apply(
+            $cart->getId(),
+            AbstractIntegrationTest::GIFTCARD,
+            AbstractIntegrationTest::GIFTCARD_PIN,
+            AbstractIntegrationTest::GIFTCARD_AMOUNT
+        );
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    #[
+        AppArea('frontend'),
+        Config(LSR::SC_SERVICE_ENABLE, AbstractIntegrationTest::LS_MAG_ENABLE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_BASE_URL, AbstractIntegrationTest::CS_URL, 'store', 'default'),
+        Config(LSR::SC_SERVICE_STORE, AbstractIntegrationTest::CS_STORE, 'store', 'default'),
+        Config(LSR::SC_SERVICE_VERSION, AbstractIntegrationTest::CS_VERSION, 'store', 'default'),
+        Config(LSR::LS_INDUSTRY_VALUE, AbstractIntegrationTest::RETAIL_INDUSTRY, 'store', 'default'),
+        DataFixture(
+            CustomerFixture::class,
+            [
+                'lsr_username' => AbstractIntegrationTest::USERNAME,
+                'lsr_id'       => AbstractIntegrationTest::LSR_ID,
+                'lsr_cardid'   => AbstractIntegrationTest::LSR_CARD_ID,
+                'lsr_token'    => AbstractIntegrationTest::CUSTOMER_ID
+            ],
+            as: 'customer'
+        ),
+        DataFixture(
+            CreateSimpleProductFixture::class,
+            [
+                LSR::LS_ITEM_ID_ATTRIBUTE_CODE => '40180',
+            ],
+            as: 'p1'
+        ),
+        DataFixture(CustomerCart::class, ['customer_id' => '$customer.id$'], 'cart1'),
+        DataFixture(AddProductToCart::class, ['cart_id' => '$cart1.id$', 'product_id' => '$p1.id$', 'qty' => 1])
+    ]
+    public function testApplyInvalidGiftCard()
+    {
+        $customer = $this->fixtures->get('customer');
+        $cart     = $this->fixtures->get('cart1');
+        $this->customerSession->setData('customer_id', $customer->getId());
+        $this->customerSession->setData(LSR::SESSION_CUSTOMER_CARDID, $customer->getLsrCardid());
+        $this->checkoutSession->setQuoteId($cart->getId());
+
+        $this->eventManager->dispatch('checkout_cart_save_after', ['items' => $cart->getAllVisibleItems()]);
+
+        $this->expectException(CouldNotSaveException::class);
+        $this->giftCardManagement->apply(
+            $cart->getId(),
+            AbstractIntegrationTest::GIFTCARD_EXPIRED,
+            AbstractIntegrationTest::GIFTCARD_EXPIRED_PIN,
+            AbstractIntegrationTest::GIFTCARD_AMOUNT
+        );
+    }
+}
