579f2764a132b2dbb6bbe0aea531c5ba8f4fe53a
Deepak K <deepakkna@lsretail.com>
2025-03-24 16:23:23 +0800

    49314 Skip catalog rule creation for discount offers with member attribute or member attribute value

--- src/Replication/Cron/DiscountCreateSetupTask.php
+++ src/Replication/Cron/DiscountCreateSetupTask.php
@@ -120,7 +120,7 @@ class DiscountCreateSetupTask
     public $message;
 
     /**
-     * @var StoreManagerInterface 
+     * @var StoreManagerInterface
      */
     public $storeManager;
 
@@ -242,6 +242,19 @@ class DiscountCreateSetupTask
                             /** @var ReplDiscountSetup $item */
                             foreach ($publishedOfferCollection as $item) {
                                 $this->deleteOfferByName($item);
+
+                                if($item->getMemberAttribute() || $item->getMemberAttributeValue()) {
+                                    $replDiscount->setData('is_failed', 0);
+                                    $replDiscount->setData('processed_at', $this->replicationHelper->getDateTime());
+                                    $replDiscount->setData('processed', '1');
+                                    $replDiscount->setData('is_updated', '0');
+                                    // @codingStandardsIgnoreStart
+                                    $this->replDiscountRepository->save($replDiscount);
+                                    // @codingStandardsIgnoreEnd
+                                    continue;
+                                }
+
+
                                 $filters  = [
                                     ['field' => 'scope_id', 'value' => $storeId, 'condition_type' => 'eq'],
                                     [
