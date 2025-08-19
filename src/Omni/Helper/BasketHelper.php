7caf5539d7a46b2821843746bb54268e20f94e2d
Umar Yousaf <umaryo@lsretail.com>
2025-08-19 15:52:56 +0800

    [#69945] Vat calculation changes for w1 customers

--- src/Omni/Helper/BasketHelper.php
+++ src/Omni/Helper/BasketHelper.php
@@ -1174,20 +1174,23 @@ class BasketHelper extends AbstractHelperOmni
             $oneList->setShipToCountryCode($country);
             $storeId = $this->getDefaultWebStore();
             $oneList->setStoreId($storeId);
+            $carrierCode = $shippingAddress->getShippingMethod();
+            $isClickCollect = $carrierCode == 'clickandcollect_clickandcollect';
 
+            if ($isClickCollect) {
+                if (!empty($pickupStore = $quote->getPickupStore())) {
+                    $oneList
+                        ->setShipToPostCode(null)
+                        ->setShipToCounty(null)
+                        ->setShipToCountryCode(null)
+                        ->setStoreId($pickupStore);
+                }
+            }
             if ($this->lsr->shipToParamsInBasketCalculationIsEnabled()) {
                 $carrierCode = $shippingAddress->getShippingMethod();
                 $isClickCollect = $carrierCode == 'clickandcollect_clickandcollect';
 
-                if ($isClickCollect) {
-                    if (!empty($pickupStore = $quote->getPickupStore())) {
-                        $oneList
-                            ->setShipToPostCode(null)
-                            ->setShipToCounty(null)
-                            ->setShipToCountryCode(null)
-                            ->setStoreId($pickupStore);
-                    }
-                } else {
+                if (!$isClickCollect) {
                     $oneList
                         ->setShipToPostCode($shippingAddress->getPostcode())
                         ->setShipToCounty($shippingAddress->getRegionCode());
