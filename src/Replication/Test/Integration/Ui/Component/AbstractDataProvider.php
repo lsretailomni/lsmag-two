63396fcb5299a1bb7f78f766b04824afb1c8b39f
Deepak K <deepakkna@lsretail.com>
2025-11-04 15:01:12 +0800

    Replication module integration script fix

--- src/Replication/Test/Integration/Ui/Component/AbstractDataProvider.php
+++ src/Replication/Test/Integration/Ui/Component/AbstractDataProvider.php
@@ -35,6 +35,43 @@ abstract class AbstractDataProvider extends TestCase
         );
     }
 
+    /**
+     * Resolve grid collection class from the data source name.
+     */
+    private function resolveGridCollectionClass(string $dataSourceName): ?string
+    {
+        $map = [
+            'ls_repl_grids_attribute_data_source' => \Ls\Replication\Model\ResourceModel\ReplAttribute\Grid\Collection::class, 
+            'ls_repl_grids_attributevalue_data_source' => \Ls\Replication\Model\ResourceModel\ReplAttributeValue\Grid\Collection::class, 
+            'ls_repl_grids_attributeoptionvalue_data_source' => \Ls\Replication\Model\ResourceModel\ReplAttributeOptionValue\Grid\Collection::class, 
+            'ls_repl_grids_barcode_data_source' => \Ls\Replication\Model\ResourceModel\ReplBarcode\Grid\Collection::class, 
+            'ls_repl_grids_country_code_data_source' => \Ls\Replication\Model\ResourceModel\ReplCountryCode\Grid\Collection::class, 
+            'ls_repl_grids_data_translation_data_source' => \Ls\Replication\Model\ResourceModel\ReplDataTranslation\Grid\Collection::class, 
+            'ls_repl_grids_discount_setup_data_source' => \Ls\Replication\Model\ResourceModel\ReplDiscountSetup\Grid\Collection::class, 
+            'ls_repl_grids_discount_validation_data_source' => \Ls\Replication\Model\ResourceModel\ReplDiscountValidation\Grid\Collection::class, 
+            'ls_repl_grids_extendedvariantvalue_data_source' => \Ls\Replication\Model\ResourceModel\ReplExtendedVariantValue\Grid\Collection::class, 
+            'ls_repl_grids_hierarchy_data_source' => \Ls\Replication\Model\ResourceModel\ReplHierarchy\Grid\Collection::class, 
+            'ls_repl_grids_hierarchyleaf_data_source' => \Ls\Replication\Model\ResourceModel\ReplHierarchyLeaf\Grid\Collection::class, 
+            'ls_repl_grids_hierarchynode_data_source' => \Ls\Replication\Model\ResourceModel\ReplHierarchyNode\Grid\Collection::class, 
+            'ls_repl_grids_imagelink_data_source' => \Ls\Replication\Model\ResourceModel\ReplImageLink\Grid\Collection::class, 
+            'ls_repl_grids_inventory_status_data_source' => \Ls\Replication\Model\ResourceModel\ReplInvStatus\Grid\Collection::class, 
+            'ls_repl_grids_item_data_source' => \Ls\Replication\Model\ResourceModel\ReplItem\Grid\Collection::class, 
+            'ls_repl_grids_item_uom_data_source' => \Ls\Replication\Model\ResourceModel\ReplItemUnitOfMeasure\Grid\Collection::class, 
+            'ls_repl_grids_itemvariant_data_source' => \Ls\Replication\Model\ResourceModel\ReplItemVariant\Grid\Collection::class, 
+            'ls_repl_grids_itemvariantregistration_data_source' => \Ls\Replication\Model\ResourceModel\ReplItemVariantRegistration\Grid\Collection::class, 
+            'ls_repl_grids_item_price_data_source' => \Ls\Replication\Model\ResourceModel\ReplPrice\Grid\Collection::class, 
+            'ls_repl_grids_item_vendor_data_source' => \Ls\Replication\Model\ResourceModel\ReplLoyVendorItemMapping\Grid\Collection::class, 
+            'ls_repl_grids_store_data_source' => \Ls\Replication\Model\ResourceModel\ReplStore\Grid\Collection::class, 
+            'ls_repl_grids_tax_setup_data_source' => \Ls\Replication\Model\ResourceModel\ReplTaxSetup\Grid\Collection::class, 
+            'ls_repl_grids_vendor_data_source' => \Ls\Replication\Model\ResourceModel\ReplVendor\Grid\Collection::class,
+            'ls_repl_grids_uom_data_source' => \Ls\Replication\Model\ResourceModel\ReplUnitOfMeasure\Grid\Collection::class,
+            'ls_repl_grids_tax_data_source' => \Ls\Replication\Model\ResourceModel\ReplTaxSetup\Grid\Collection::class,
+            // Add others as needed
+        ];
+
+        return $map[$dataSourceName] ?? null;
+    }
+
     public function assertDataExists()
     {
         $data = $this->dataProvider->getData();
@@ -55,13 +92,15 @@ abstract class AbstractDataProvider extends TestCase
     /**
      * @return array
      */
-    public function getDataByIdProvider(): array
+    public static function getDataByIdProvider(): array
     {
+        $reflection = new \ReflectionClass(static::class);
+        $self = $reflection->newInstanceWithoutConstructor();
         return [
             [[
                  'condition_type' => 'eq',
-                 'field' => $this->getSearchFieldName(),
-                 'value' => $this->getSearchFieldValue()
+                 'field' => $self->getSearchFieldName(),
+                 'value' => $self->getSearchFieldValue()
              ]],
         ];
     }
