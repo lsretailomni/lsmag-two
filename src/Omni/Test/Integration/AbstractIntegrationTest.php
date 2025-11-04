a2b93dab3bdedb4ec41f837ec91cdfcfd1403da7
Umar Yousaf <umaryo@lsretail.com>
2025-11-04 11:16:57 +0800

    [#69817] Fixed error while placing order

--- src/Omni/Test/Integration/AbstractIntegrationTest.php
+++ src/Omni/Test/Integration/AbstractIntegrationTest.php
@@ -58,7 +58,7 @@ if (!defined("PICKUP_TIMESLOTS_ENABLED")) { define('PICKUP_TIMESLOTS_ENABLED', g
 if (!defined("SC_CLICKCOLLECT_ENABLED")) { define('SC_CLICKCOLLECT_ENABLED', getenv('SC_CLICKCOLLECT_ENABLED')); }
 if (!defined("TENDER_TYPE_MAPPINGS")) { define('TENDER_TYPE_MAPPINGS', getenv('TENDER_TYPE_MAPPINGS')); }
 if (!defined("ADYEN_RESPONSE")) { define('ADYEN_RESPONSE', json_decode(getenv('ADYEN_RESPONSE'), true)); }
-if (!defined("LS_ORDER_NUMBER_PREFIX_PATH")) { define('LS_ORDER_NUMBER_PREFIX_PATH', json_decode(getenv('LS_ORDER_NUMBER_PREFIX_PATH'), true)); }
+if (!defined("LS_ORDER_NUMBER_PREFIX_PATH")) { define('LS_ORDER_NUMBER_PREFIX_PATH', getenv('LS_ORDER_NUMBER_PREFIX_PATH')); }
 
 class AbstractIntegrationTest extends TestCase
 {
