f97d27b565309feb11aca60cb9f956f445c930b2
Umar Yousaf <umaryo@lsretail.com>
2025-12-08 11:11:13 +0800

    [#75469] Pipeline test

---
+++ src/Customer/Plugin/Sales/Paypal/Braintree/StoreConfigResolverPlugin.php
@@ -0,0 +1,42 @@
+<?php
+namespace Ls\Customer\Plugin\Sales\Paypal\Braintree;
+
+use PayPal\Braintree\Model\StoreConfigResolver;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
+
+class StoreConfigResolverPlugin
+{
+    /**
+     * @param StoreManagerInterface $storeManager
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        public StoreManagerInterface $storeManager,
+        public LoggerInterface $logger,
+    ) {
+    }
+
+    /**
+     * Around plugin to fix NoSuchEntityException when order ID is invalid
+     *
+     * @param StoreConfigResolver $subject
+     * @param \Closure $proceed
+     * @return false|int|mixed
+     */
+    public function aroundGetStoreId(StoreConfigResolver $subject, \Closure $proceed)
+    {
+        try {
+            return $proceed();
+        } catch (NoSuchEntityException $e) {
+            // If order_id is not a valid Magento order, fall back to current store
+            try {
+                return (int)$this->storeManager->getStore()->getId();
+            } catch (\Exception $e) {
+                $this->logger->error('Unable to determine fallback store ID: ' . $e->getMessage());
+                return false;
+            }
+        }
+    }
+}
