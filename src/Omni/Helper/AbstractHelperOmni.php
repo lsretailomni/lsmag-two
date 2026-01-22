e59ae50540966027ae5fcbe4b3a3c49fca90c74b
Umar Yousaf <umaryo@lsretail.com>
2026-01-22 12:32:52 +0800

    [#77120] Removed unncessary calls to Central

--- src/Omni/Helper/AbstractHelperOmni.php
+++ src/Omni/Helper/AbstractHelperOmni.php
@@ -3,6 +3,8 @@
 namespace Ls\Omni\Helper;
 
 use \Ls\Core\Model\LSR;
+use \Ls\Replication\Api\ReplStoreRepositoryInterface;
+use \Ls\Replication\Api\ReplStoreTenderTypeRepositoryInterface;
 use Magento\Catalog\Model\ProductFactory;
 use Magento\Catalog\Model\ProductRepository;
 use Magento\Checkout\Model\Cart;
@@ -13,161 +15,28 @@ use Magento\Customer\Model\CustomerFactory;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Directory\Model\Currency;
 use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem\Driver\File;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Pricing\Helper\Data as PriceHelper;
 use Magento\Framework\Registry;
 use Magento\Framework\Session\SessionManagerInterface;
 use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Model\MaskedQuoteIdToQuoteIdInterface;
 use Magento\Quote\Model\ResourceModel\Quote;
 use Magento\Quote\Model\ResourceModel\Quote\Item;
+use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
 
 /**
  * Abstract Helper for merging common data members and member functions
  */
 class AbstractHelperOmni extends AbstractHelper
 {
-    /**
-     * @var CustomerFactory
-     */
-    public $customerFactory;
-
-    /**
-     * @var CustomerSession
-     */
-    public $customerSession;
-
-    /**
-     * @var Filesystem
-     */
-    public $filesystem;
-
-    /**
-     * @var CheckoutSession
-     */
-    public $checkoutSession;
-
-    /**
-     * @var GroupRepositoryInterface
-     */
-    public $groupRepository;
-
-    /**
-     * @var LSR
-     */
-    public $lsr;
-
-    /**
-     * @var Currency
-     */
-    public $currencyHelper;
-
-    /**
-     * @var BasketHelper
-     */
-    public $basketHelper;
-
-    /**
-     * @var CacheHelper
-     */
-    public $cacheHelper;
-
-    /**
-     * @var ContactHelper
-     */
-    public $contactHelper;
-
-    /**
-     * @var Data
-     */
-    public $dataHelper;
-
-    /**
-     * @var GiftCardHelper
-     */
-    public $giftCardHelper;
-
-    /**
-     * @var ItemHelper
-     */
-    public $itemHelper;
-
-    /**
-     * @var LoyaltyHelper
-     */
-    public $loyaltyHelper;
-
-    /**
-     * @var OrderHelper
-     */
-    public $orderHelper;
-
-    /**
-     * @var SessionHelper
-     */
-    public $sessionHelper;
-
-    /**
-     * @var StockHelper
-     */
-    public $stockHelper;
-
-    /**
-     * @var StoreHelper
-     */
-    public $storeHelper;
-
-    /** @var Cart $cart */
-    public $cart;
-
-    /** @var ProductRepository $productRepository */
-    public $productRepository;
-
-    /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
-    public $searchCriteriaBuilder;
-
-    /**
-     * @var Configurable
-     */
-    public $catalogProductTypeConfigurable;
-
-    /** @var ProductFactory $productFactory */
-    public $productFactory;
-
-    /** @var Registry $registry */
-    public $registry;
-
-    /**
-     * @var SessionManagerInterface
-     */
-    public $session;
-
-    /**
-     * @var $quoteRepository
-     */
-    public $quoteRepository;
-
-    /**
-     * @var Quote
-     */
-    public $quoteResourceModel;
-
-    /**
-     * @var CartRepositoryInterface
-     */
-    public $cartRepository;
-
-    /**
-     * @var DateTime
-     */
-    public $dateTime;
-
-    /**
-     * @var Item
-     */
-    public $itemResourceModel;
-
     /**
      * @param Context $context
      * @param BasketHelper $basketHelper
@@ -184,7 +53,7 @@ class AbstractHelperOmni extends AbstractHelper
      * @param CustomerFactory $customerFactory
      * @param CustomerSession $customerSession
      * @param CheckoutSession $checkoutSession
-     * @param Filesystem $Filesystem
+     * @param Filesystem $filesystem
      * @param GroupRepositoryInterface $groupRepository
      * @param LSR $lsr
      * @param Currency $currencyHelper
@@ -200,71 +69,59 @@ class AbstractHelperOmni extends AbstractHelper
      * @param CartRepositoryInterface $cartRepository
      * @param DateTime $dateTime
      * @param Item $itemResourceModel
+     * @param ReplStoreRepositoryInterface $storeRepository
+     * @param ManagerInterface $messageManager
+     * @param PriceHelper $priceHelper
+     * @param WriterInterface $configWriter
+     * @param DirectoryList $directoryList
+     * @param ReplStoreTenderTypeRepositoryInterface $replStoreTenderTypeRepository
+     * @param GetCartForUser $getCartForUser
+     * @param MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteId
+     * @param File $fileSystemDriver
      */
     public function __construct(
-        Context                  $context,
-        BasketHelper             $basketHelper,
-        CacheHelper              $cacheHelper,
-        ContactHelper            $contactHelper,
-        Data                     $dataHelper,
-        GiftCardHelper           $giftCardHelper,
-        ItemHelper               $itemHelper,
-        LoyaltyHelper            $loyaltyHelper,
-        OrderHelper              $orderHelper,
-        SessionHelper            $sessionHelper,
-        StockHelper              $stockHelper,
-        StoreHelper              $storeHelper,
-        CustomerFactory          $customerFactory,
-        CustomerSession          $customerSession,
-        CheckoutSession          $checkoutSession,
-        Filesystem               $Filesystem,
-        GroupRepositoryInterface $groupRepository,
-        LSR                      $lsr,
-        Currency                 $currencyHelper,
-        Cart                     $cart,
-        ProductRepository        $productRepository,
-        SearchCriteriaBuilder    $searchCriteriaBuilder,
-        Configurable             $catalogProductTypeConfigurable,
-        ProductFactory           $productFactory,
-        Registry                 $registry,
-        SessionManagerInterface  $session,
-        CartRepositoryInterface  $quoteRepository,
-        Quote                    $quoteResourceModel,
-        CartRepositoryInterface  $cartRepository,
-        DateTime                 $dateTime,
-        Item                     $itemResourceModel
+        Context $context,
+        public BasketHelper $basketHelper,
+        public CacheHelper $cacheHelper,
+        public ContactHelper $contactHelper,
+        public Data $dataHelper,
+        public GiftCardHelper $giftCardHelper,
+        public ItemHelper $itemHelper,
+        public LoyaltyHelper $loyaltyHelper,
+        public OrderHelper $orderHelper,
+        public SessionHelper $sessionHelper,
+        public StockHelper $stockHelper,
+        public StoreHelper $storeHelper,
+        public CustomerFactory $customerFactory,
+        public CustomerSession $customerSession,
+        public CheckoutSession $checkoutSession,
+        public Filesystem $filesystem,
+        public GroupRepositoryInterface $groupRepository,
+        public LSR $lsr,
+        public Currency $currencyHelper,
+        public Cart $cart,
+        public ProductRepository $productRepository,
+        public SearchCriteriaBuilder $searchCriteriaBuilder,
+        public Configurable $catalogProductTypeConfigurable,
+        public ProductFactory $productFactory,
+        public Registry $registry,
+        public SessionManagerInterface $session,
+        public CartRepositoryInterface $quoteRepository,
+        public Quote $quoteResourceModel,
+        public CartRepositoryInterface $cartRepository,
+        public DateTime $dateTime,
+        public Item $itemResourceModel,
+        public ReplStoreRepositoryInterface $storeRepository,
+        public ManagerInterface $messageManager,
+        public PriceHelper $priceHelper,
+        public WriterInterface $configWriter,
+        public DirectoryList $directoryList,
+        public ReplStoreTenderTypeRepositoryInterface $replStoreTenderTypeRepository,
+        public GetCartForUser $getCartForUser,
+        public MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteId,
+        public File $fileSystemDriver
     ) {
         parent::__construct($context);
-        $this->basketHelper                   = $basketHelper;
-        $this->cacheHelper                    = $cacheHelper;
-        $this->contactHelper                  = $contactHelper;
-        $this->dataHelper                     = $dataHelper;
-        $this->giftCardHelper                 = $giftCardHelper;
-        $this->itemHelper                     = $itemHelper;
-        $this->loyaltyHelper                  = $loyaltyHelper;
-        $this->orderHelper                    = $orderHelper;
-        $this->sessionHelper                  = $sessionHelper;
-        $this->stockHelper                    = $stockHelper;
-        $this->storeHelper                    = $storeHelper;
-        $this->customerFactory                = $customerFactory;
-        $this->customerSession                = $customerSession;
-        $this->checkoutSession                = $checkoutSession;
-        $this->filesystem                     = $Filesystem;
-        $this->groupRepository                = $groupRepository;
-        $this->lsr                            = $lsr;
-        $this->currencyHelper                 = $currencyHelper;
-        $this->cart                           = $cart;
-        $this->productRepository              = $productRepository;
-        $this->searchCriteriaBuilder          = $searchCriteriaBuilder;
-        $this->catalogProductTypeConfigurable = $catalogProductTypeConfigurable;
-        $this->productFactory                 = $productFactory;
-        $this->registry                       = $registry;
-        $this->session                        = $session;
-        $this->quoteRepository                = $quoteRepository;
-        $this->quoteResourceModel             = $quoteResourceModel;
-        $this->cartRepository                 = $cartRepository;
-        $this->dateTime                       = $dateTime;
-        $this->itemResourceModel              = $itemResourceModel;
         $this->initialize();
     }
 
