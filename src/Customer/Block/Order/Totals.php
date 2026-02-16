f75524a39a10e2985744b1bd4d510afce1ec4fe9
Deepak K <deepakkna@lsretail.com>
2026-02-16 12:29:59 +0800

    77960 order view fix

--- src/Customer/Block/Order/Totals.php
+++ src/Customer/Block/Order/Totals.php
@@ -217,7 +217,18 @@ class Totals extends AbstractOrderBlock
         $order                = $this->getOrder();
         $requiredTransaction  = [];
         if ($order) {
-            $documentId           = $order->getLscMemberSalesBuffer()->getDocumentId() ?? $this->_request->getParam('order_id');
+            if (is_array($order->getLscMemberSalesBuffer())) {
+                foreach ($order->getLscMemberSalesBuffer() as $saleLine) {
+                    if($saleLine->getDocumentSourceType() == 1) {
+                        $documentId = $saleLine->getDocumentId();
+                        break;
+                    } else {
+                        $documentId = $this->_request->getParam('order_id');
+                    }
+                }
+            } else {
+                $documentId = $order->getLscMemberSalesBuffer()->getDocumentId() ?? $this->_request->getParam('order_id');
+            }
             $newDocumentId        = $this->_request->getParam('new_order_id');
             $newDocumentId        = ($newDocumentId) ? [$newDocumentId] : [];
             
