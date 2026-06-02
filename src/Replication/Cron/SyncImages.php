4f34f7dd66d75c3162cd18204fb8a80fbf763c5c
Deepak K <deepakkna@lsretail.com>
2026-06-02 19:30:33 +0800

    #82224 - Fixes

--- src/Replication/Cron/SyncImages.php
+++ src/Replication/Cron/SyncImages.php
@@ -654,7 +654,8 @@ class SyncImages extends ProductCreateTask
         $filters  = [
             ['field' => 'main_table.TableName', 'value' => 'Item%', 'condition_type' => 'like'],
             ['field' => 'main_table.TableName', 'value' => 'Item Category', 'condition_type' => 'neq'],
-            ['field' => 'main_table.scope_id', 'value' => $this->getScopeId(), 'condition_type' => 'eq']
+            ['field' => 'main_table.scope_id', 'value' => $this->getScopeId(), 'condition_type' => 'eq'],
+            ['field' => 'main_table.ImageId', 'value' => true, 'condition_type' => 'notnull']
         ];
         $criteria = $this->replicationHelper->buildCriteriaForArrayWithAlias(
             $filters,
