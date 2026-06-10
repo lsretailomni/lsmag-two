bc1dd5019066316cdc878cc0452e76b052ff94bc
Deepak K <deepakkna@lsretail.com>
2026-06-10 15:30:54 +0800

    #82770 - Hyva theme recent order changes

--- src/Customer/Block/Order/Hyva/Info.php
+++ src/Customer/Block/Order/Hyva/Info.php
@@ -3,6 +3,19 @@ declare(strict_types=1);
 
 namespace Ls\Customer\Block\Order\Hyva;
 
+use \Ls\Core\Model\LSR;
+use \Ls\Omni\Helper\Data as DataHelper;
+use \Ls\Omni\Helper\LoyaltyHelper;
+use \Ls\Omni\Helper\OrderHelper;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Directory\Model\CountryFactory;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Pricing\Helper\Data as PriceHelper;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Sales\Model\OrderRepository;
+use Magento\Framework\App\Http\Context as HttpContext;
+
 /**
  * This class is overriding in hospitality module
  *
