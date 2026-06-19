83b04739538abc2561e05d56f1a650efb3c75d5b
Deepak K <deepakkna@lsretail.com>
2026-06-19 12:51:57 +0800

    [#82770] - Add Hyvä theme support to customer registration form
    - Add HyvaThemeResolver to detect Hyvä themes via theme inheritance chain
    - Switch Name/Telephone widget plugins to Hyvä templates when active
    - Add Hyvä-styled name/telephone widget templates preserving validation
    - Re-add CAPTCHA and e-mail maxlength on Hyvä registration layout

--- src/Customer/Plugin/Customer/Block/Widget/Name.php
+++ src/Customer/Plugin/Customer/Block/Widget/Name.php
@@ -2,10 +2,24 @@
 
 namespace Ls\Customer\Plugin\Customer\Block\Widget;
 
+use Ls\Customer\Model\HyvaThemeResolver;
+
 class Name
 {
+    /**
+     * @param HyvaThemeResolver $hyvaThemeResolver
+     */
+    public function __construct(
+        private HyvaThemeResolver $hyvaThemeResolver
+    )
+    {}
+
     public function beforeToHtml(\Magento\Customer\Block\Widget\Name $subject)
     {
-        $subject->setTemplate('Ls_Customer::widget/name.phtml');
+        $template = $this->hyvaThemeResolver->isHyva()
+            ? 'Ls_Customer::widget/hyva/name.phtml'
+            : 'Ls_Customer::widget/name.phtml';
+
+        $subject->setTemplate($template);
     }
 }
